c9ecfd548b6ca5f91e3d4d63889c4d1ba7a8c6d4
Sergey Kalinkin <italia80@yandex.ru>
2022-01-24 21:15:14 +0200

    Diplom_1

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,4 +1,12 @@
-import static org.junit.Assert.*;
+package praktikum;
+
+import org.junit.Test;
+
 public class IngredientTypeTest {
-  
+
+    @Test
+    public void valueOf() {
+        IngredientType.valueOf("SAUCE");
+        IngredientType.valueOf("FILLING");
+    }
 }
