e3a7116e1a66171c93e294a645763d10d2fca174
buyan.fred@gmail.com <buyan.fred@gmail.com>
2022-01-27 16:43:20 +0300

    Добавлены тесты для Ingredient

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -5,11 +5,12 @@ import praktikum.IngredientType;
 import static org.junit.Assert.assertEquals;
 
 public class IngredientTest {
-    @Mock
+
     IngredientType inredientType;
     final float PRICE = 1f;
     final String NAME = "name";
-    Ingredient sauce = new Ingredient(inredientType,NAME,PRICE);
+    Ingredient sauce = new Ingredient(IngredientType.SAUCE,NAME,PRICE);
+    Ingredient filling = new Ingredient(IngredientType.FILLING,NAME,PRICE);
     @Test
     public void testGetPrice(){
         assertEquals("Проверка стоимости",PRICE, sauce.getPrice(),0);
@@ -17,5 +18,13 @@ public class IngredientTest {
     @Test
     public void testGetName(){
         assertEquals("Проверка имени",NAME,sauce.getName());
+    }
+    @Test
+    public void testTypeSauce(){
+        assertEquals("Проверка типа соус", IngredientType.SAUCE,sauce.getType());
+    }
+    @Test
+    public void testTypeFilling(){
+        assertEquals("Проверка типа начинка", IngredientType.FILLING,filling.getType());
     }
 }
