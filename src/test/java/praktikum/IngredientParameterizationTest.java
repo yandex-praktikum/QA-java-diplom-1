2eef9071943ca0d5d4a86b523c6888420bcb4c23
Artem Parich <parich-a@yandex.ru>
2022-02-12 12:15:54 +0300

    add Test for class Ingredient and IngredientType

---
+++ src/test/java/praktikum/IngredientParameterizationTest.java
@@ -0,0 +1,37 @@
+package praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IngredientParameterizationTest {
+    @Mock
+    float priceMock;
+    String nameMock;
+
+    private final String expected;
+    IngredientType ingredientType;
+
+    public IngredientParameterizationTest(IngredientType ingredientType, String expected) {
+        this.ingredientType = ingredientType;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getIngredientType() {
+        return new Object[][] {
+                {IngredientType.SAUCE, "SAUCE"},
+                {IngredientType.FILLING, "FILLING"},
+        };
+    }
+
+    @Test
+    public void doesHaveManeTrueResult() {
+        Ingredient ingredient = new Ingredient(ingredientType, nameMock, priceMock);
+        assertEquals(ingredient.getType().toString(), expected);
+    }
+}
