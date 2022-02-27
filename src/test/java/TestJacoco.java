3452ff49d27f7eb28d6abc770f0933d1537b9127
Chervonnyy Anton <anton@chervonnyy.ru>
2022-02-27 12:42:28 +0100

    first commit

---
+++ src/test/java/TestJacoco.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestJacoco {
+
+    @Test
+    public void testSalaryUnderLimit() {
+
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void testSalaryOverLimit() {
+        Assert.assertEquals(1,1);
+    }
+}
