3705d7fcf4084a5e534ac844d710947678c0dd16
Васильев Данила <vasilyevdanila@MacBook-Air-Vasilev.local>
2022-03-19 00:07:55 +0300

    added BunTest

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    @Test
+    public void getNameTest() {
+
+        String expected = "Булочка";
+        Bun bun = new Bun("Булочка", 100);
+
+        String actual = bun.getName();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPriceTest() {
+
+        float expected = (float) 100.00;
+        Bun bun = new Bun("Булочка", (float) 100.00);
+
+        float actual = bun.getPrice();
+
+        assertEquals(expected, actual, 0);
+    }
+}
