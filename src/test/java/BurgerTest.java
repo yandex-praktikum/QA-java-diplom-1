5e04966660358e27ea3c6546270d70961cab4db4
Tamara Konashenkova <te.konashenkova@yandex.ru>
2022-04-07 21:33:41 +0300

    Исправления по замечаниям ревьюера

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,12 +1,11 @@
+
 import org.junit.Before;
 import org.junit.Test;
 import praktikum.*;
 import java.util.List;
 import static org.junit.Assert.*;
 
-
 public class BurgerTest {
-
     public Database database = new Database();
     List<Bun> bunsData = database.availableBuns();
     List<Ingredient> ingredientsData = database.availableIngredients();
@@ -20,21 +19,6 @@ public class BurgerTest {
             ingredientsData.add(new Ingredient(IngredientType.SAUCE, "hot sauce", 100));
             ingredientsData.add(new Ingredient(IngredientType.FILLING, "cutlet", 100));
         }
-    }
-
-    @Test
-    public void setBunsTest() {
-        Burger burger = new Burger();
-        burger.setBuns(bunsData.get(0));
-        assertEquals(bunsData.get(0).name, burger.bun.name);
-        assertEquals(bunsData.get(0).price, burger.bun.price, 0);
-    }
-
-    @Test
-    public void addIngredientTest() {
-        Burger burger = new Burger();
-        burger.addIngredient(ingredientsData.get(0));
-        assertTrue(burger.ingredients.contains(ingredientsData.get(0)));
     }
 
     @Test
@@ -56,3 +40,4 @@ public class BurgerTest {
         assertEquals(moveIngredient, actualIngredient);
     }
 }
+
--- src/test/java/IngredientParameterizedTest.java
+++ src/test/java/IngredientParameterizedTest.java
@@ -18,7 +18,7 @@ public class IngredientParameterizedTest {
         this.expected = expected;
     }
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "Тестовые данные: {0} {1}")
     public static Object[][] getSumData() {
         return new Object[][]{
                 { SAUCE, SAUCE},
