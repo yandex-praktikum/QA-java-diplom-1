ecc210ebb59099b32105840155169d20d1f8737e
TatianaKydryashova <kudryashova_ts@mail.ru>
2022-04-22 12:24:06 +0300

    add progect

--- src/main/java/praktikum/Ingredient.java
+++ src/main/java/praktikum/Ingredient.java
@@ -5,6 +5,7 @@ package praktikum;
  * Ингредиент: начинка или соус.
  * У ингредиента есть тип (начинка или соус), название и цена.
  */
+
 public class Ingredient {
 
     public IngredientType type;
