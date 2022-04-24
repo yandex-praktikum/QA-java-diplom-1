27e51882ffbd4ed48679aff9080a74723b2c69c2
Pavel Lonshakov <l-on-shak-off@yandex.ru>
2022-04-24 20:48:15 +0400

    move IngredientTypeTest

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,19 +1,33 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class IngredientTypeTest {
 
-    @Test
-    public void sauceTest() {
-        assertEquals("SAUCE", IngredientType.valueOf("SAUCE").toString());
+    String expectedIngredientType;
+    String actualType;
+
+    public IngredientTypeTest(String expectedIngredientType, String actualType) {
+        this.expectedIngredientType = expectedIngredientType;
+        this.actualType = actualType;
+    }
+
+    @Parameterized.Parameters(name = "{index}:IngredientType {1} exist")
+    public static Object[][] getIngredientType() {
+        return new Object[][]{
+                {"SAUCE", "SAUCE"},
+                {"FILLING", "FILLING"}
+        };
     }
 
     @Test
     public void fillingTest() {
-        assertEquals("FILLING", IngredientType.valueOf("FILLING").toString());
+        assertEquals(expectedIngredientType, IngredientType.valueOf(actualType).toString());
     }
 
 }
