ed9cca6833b5f5800fe2b65222cd4cf1a628fda2
Fedor Fedorov <fedor0098@gmail.com>
2022-05-05 20:16:02 +0200

    Fixed problems after first review

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.*;
 
@@ -10,13 +11,16 @@ import static org.junit.Assert.*;
 public class BurgerTest {
 
     Burger burger = new Burger();
-    Bun bun = new Bun("black bun", 100);
+    Database database = new Database();
 
     @Mock
     Ingredient ingredient;
+    @Mock
+    Bun bun;
 
     @Test
     public void setBunTest() {
+        Bun bun = new Bun(database.availableBuns().get(0).name, database.availableBuns().get(0).price);
         burger.setBuns(bun);
         assertNotNull("Нет булки",burger.bun);
     }
@@ -24,45 +28,83 @@ public class BurgerTest {
     @Test
     public void addIngredientTest() {
         burger.addIngredient(ingredient);
-        assertEquals("Ингридиент не был добавлен",1, burger.ingredients.size());
+        assertEquals("Ингредиент не был добавлен",1, burger.ingredients.size());
     }
 
     @Test
     public void removeIngredientTest() {
         burger.addIngredient(ingredient);
         burger.removeIngredient(0);
-        assertEquals("Ингридиент не был удален",0, burger.ingredients.size());
+        assertEquals("Ингредиент не был удален",0, burger.ingredients.size());
     }
 
     @Test
     public void moveIngredientTest() {
-        burger.addIngredient(new Ingredient(IngredientType.SAUCE, "Соус Spicy-X", 90));
-        burger.addIngredient(new Ingredient(IngredientType.FILLING, "Говяжий метеорит", 3000));
+        burger.addIngredient(new Ingredient(database.availableIngredients().get(0).type, database.availableIngredients().get(0).name, database.availableIngredients().get(0).price));
+        burger.addIngredient(new Ingredient(database.availableIngredients().get(1).type, database.availableIngredients().get(1).name, database.availableIngredients().get(1).price));
         burger.moveIngredient(0, 1);
-        assertEquals("Ингридиенты не поменяли свои индексы", "Говяжий метеорит", burger.ingredients.get(0).name);
+        assertEquals("Ингредиенты не поменяли свои индексы", database.availableIngredients().get(1).name, burger.ingredients.get(0).name);
     }
 
     @Test
     public void getPriceTest(){
-        Database database = new Database();
         burger.setBuns(database.availableBuns().get(0));
         float bunPrice = database.availableBuns().get(0).price;
         burger.addIngredient(database.availableIngredients().get(0));
         float ingredientPrice = database.availableIngredients().get(0).price;
-        float correctPrice = (bunPrice*2) + ingredientPrice;
-        assertEquals("Цена не считается",correctPrice, burger.getPrice(),0.000001);
+        float correctPrice = (bunPrice * 2) + ingredientPrice;
+        assertEquals("Цена не считается",correctPrice, burger.getPrice(),0);
+    }
+
+    @Test
+    public void getErrorWithBiggerPriceForBunTest(){
+        burger.setBuns(database.availableBuns().get(0));
+        Mockito.when(bun.getPrice()).thenReturn(101F);
+        float bunPrice = bun.getPrice();
+        float ingredientPrice = 0;
+        float correctPrice = (bunPrice * 2) + ingredientPrice;
+        assertNotEquals("Цена одинаковая",correctPrice, burger.getPrice(),0);
+    }
+
+    @Test
+    public void getErrorWithLowerPriceForBunTest(){
+        burger.setBuns(database.availableBuns().get(0));
+        Mockito.when(bun.getPrice()).thenReturn(99F);
+        float bunPrice = bun.getPrice();
+        float ingredientPrice = 0;
+        float correctPrice = (bunPrice * 2) + ingredientPrice;
+        assertNotEquals("Цена одинаковая",correctPrice, burger.getPrice(),0);
+    }
+
+    @Test
+    public void getErrorWithBiggerPriceForIngredientTest(){
+        burger.setBuns(database.availableBuns().get(0));
+        float bunPrice = bun.getPrice();
+        Mockito.when(ingredient.getPrice()).thenReturn(101F);
+        float ingredientPrice = ingredient.getPrice();
+        float correctPrice = (bunPrice * 2) + ingredientPrice;
+        assertNotEquals("Цена одинаковая",correctPrice, burger.getPrice(),0);
+    }
+
+    @Test
+    public void getErrorWithLowerPriceForIngredientTest(){
+        burger.setBuns(database.availableBuns().get(0));
+        float bunPrice = bun.getPrice();
+        Mockito.when(ingredient.getPrice()).thenReturn(99F);
+        float ingredientPrice = ingredient.getPrice();
+        float correctPrice = (bunPrice * 2) + ingredientPrice;
+        assertNotEquals("Цена одинаковая",correctPrice, burger.getPrice(),0);
     }
 
     @Test
     public void receiptHasBunNameTest(){
-        Database database = new Database();
         burger.setBuns(database.availableBuns().get(0));
         String bunName = database.availableBuns().get(0).name;
         assertTrue("Нет булочки в чеке", burger.getReceipt().contains(bunName));
     }
+
     @Test
     public void receiptHasBunPriceTest(){
-        Database database = new Database();
         burger.setBuns(database.availableBuns().get(0));
         int burgerPrice = (int)burger.getPrice();
         assertTrue("Нет названия в чеке", burger.getReceipt().contains(Integer.toString(burgerPrice)));
@@ -70,7 +112,6 @@ public class BurgerTest {
 
     @Test
     public void receiptHasIngredientTypeTest(){
-        Database database = new Database();
         burger.setBuns(bun);
         burger.addIngredient(database.availableIngredients().get(0));
         String ingredientType = database.availableIngredients().get(0).type.toString();
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,25 +1,67 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.*;
 
 import static org.junit.Assert.*;
 import static praktikum.IngredientType.*;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class IngredientTest {
 
-    Ingredient ingredient = new Ingredient(SAUCE, "Соус Spicy-X", 90);
+    @Mock
+    Ingredient ingredient;
+
+    Database database = new Database();
 
     @Test
     public void getPriceTest() {
-        assertEquals("Неправильная цена",90, ingredient.getPrice(), 0);
+        Mockito.when(ingredient.getPrice()).thenReturn(100F);
+        assertEquals("Цена не совпадает", database.availableIngredients().get(0).price, ingredient.getPrice(), 0.0);
+    }
+
+    @Test
+    public void getErrorPriceWithBiggerPriceTest() {
+        Mockito.when(ingredient.getPrice()).thenReturn(101F);
+        assertTrue("Цена совпадает",database.availableIngredients().get(0).price != ingredient.getPrice());
+    }
+
+    @Test
+    public void getErrorPriceWithLessPriceTest() {
+        Mockito.when(ingredient.getPrice()).thenReturn(99F);
+        assertTrue("Цена совпадает",database.availableIngredients().get(0).price != ingredient.getPrice());
     }
 
     @Test
     public void getNameTest() {
-        assertEquals( "Неправильное имя","Соус Spicy-X", ingredient.getName());
+        Mockito.when(ingredient.getName()).thenReturn("hot sauce");
+        assertEquals("Неправильное имя",database.availableIngredients().get(0).name, ingredient.getName());
+    }
+
+    @Test
+    public void getWrongNameTest() {
+        Mockito.when(ingredient.getName()).thenReturn("hot chili");
+        assertNotEquals( "Имя совпало",database.availableIngredients().get(0).name, ingredient.getName());
+    }
+
+    @Test
+    public void getNullNameTest() {
+        Mockito.when(ingredient.getName()).thenReturn(null);
+        assertNotEquals( "Имя совпало",database.availableIngredients().get(0).name, ingredient.getName());
     }
 
     @Test
     public void getTypeTest() {
-        assertEquals("Неправильный тип",SAUCE, ingredient.getType());
+        Mockito.when(ingredient.getType()).thenReturn(SAUCE);
+        assertEquals("Неправильный тип",database.availableIngredients().get(0).type, ingredient.getType());
+    }
+
+    @Test
+    public void getWrongTypeTest() {
+        Mockito.when(ingredient.getType()).thenReturn(FILLING);
+        assertNotEquals("Тип совпадает",database.availableIngredients().get(0).type, ingredient.getType());
     }
 }
