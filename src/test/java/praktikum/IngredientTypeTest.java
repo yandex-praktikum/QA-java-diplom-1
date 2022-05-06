837f8741759bcf3718f0a768989a2699879ce582
ansiola <ansiola@mail.ru>
2022-05-06 12:51:10 +0300

    commit with jacoco report

---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,18 @@
+package praktikum;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTypeTest {
+    @Test
+    public void valueOf() {
+
+        String actual = Arrays.toString(IngredientType.values());
+        String expected = Arrays.toString(new String [] {"SAUCE","FILLING"});
+
+        assertEquals("The ENUM value is not correct",expected,actual);
+    }
+}
