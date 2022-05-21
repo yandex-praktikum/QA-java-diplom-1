e84d36a0b225dea26a56e111080e14a0638f4cd8
Lidkin <lidkin79@gmail.com>
2022-05-21 13:13:24 +0300

    added changes to pom.xml and made changes to the burger price format in the Burger.java in getReceipt() so that two numbers are displayed after the dot

--- src/main/java/praktikum/Burger.java
+++ src/main/java/praktikum/Burger.java
@@ -49,7 +49,7 @@ public class Burger {
         }
 
         receipt.append(String.format("(==== %s ====)%n", bun.getName()));
-        receipt.append(String.format("%nPrice: %f%n", getPrice()));
+        receipt.append(String.format("%nPrice: %,.2f%n", getPrice()));
 
         return receipt.toString();
     }
