ec71edf5e78b0867301abc87fb2af7b78f7a9838
Nastya Olennikova <olennikovanastyaa@yandex.ru>
2022-05-22 13:11:42 +0300

    BunTests added

---
+++ src/test/java/BunTests.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class BunTests {
+    private final String name;
+    private final float price;
+
+    public BunTests(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters // добавили аннотацию
+    public static Object[][] getBunData() {
+        return new Object[][]{
+                {"Ржаная", 100},
+                {"Сырная", 99.99f},
+                {"Sesame", 115.35f},
+                {"Классическая", 150}
+        };
+    }
+
+    @Test
+    public void isBunDataValid() {
+        Bun bun = new Bun(name, price);
+        assertNotNull(bun.getName());
+        assertTrue(bun.getPrice() > 0);
+    }
+}
+
