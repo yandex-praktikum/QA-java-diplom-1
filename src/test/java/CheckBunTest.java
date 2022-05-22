09529d25073e11501af67310f3daf497869a17a2
Sofia Kozlova <kozlovasonya@gmail.com>
2022-05-22 23:15:45 +0300

    Diplom 1

--- src/test/java/CheckBunTest.java
+++ src/test/java/CheckBunTest.java
@@ -4,16 +4,16 @@ import praktikum.Bun;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class BunTest {
+public class CheckBunTest {
     @Test
     public void getNameTest() {
-        Bun bun =new Bun("test",1000);
+        Bun bun = new Bun("test", 1000);
         assertEquals("test", bun.getName());
     }
+
     @Test
     public void getPriceTest() {
-        Bun bun =new Bun("test",1000);
+        Bun bun = new Bun("test", 1000);
         assertEquals(1000.0, bun.getPrice());
     }
-    //дописать параметризованный тест
 }
--- src/test/java/CheckIngredientTest.java
+++ src/test/java/CheckIngredientTest.java
@@ -4,22 +4,22 @@ import praktikum.Ingredient;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static praktikum.IngredientType.SAUCE;
 
-public class IngredientTest {
+public class CheckIngredientTest {
     @Test
     public void getIngredientPriceTest() {
-        Ingredient ingredient =new Ingredient(SAUCE,"test",1000);
-        assertEquals(1000.0, ingredient.getPrice());
+        Ingredient ingredient = new Ingredient(SAUCE, "test", 1000);
+        assertEquals(1000, ingredient.getPrice(), 0);
     }
 
     @Test
     public void getIngredientNameTest() {
-        Ingredient ingredient =new Ingredient(SAUCE,"test",1000);
+        Ingredient ingredient = new Ingredient(SAUCE, "test", 1000);
         assertEquals("test", ingredient.getName());
     }
 
     @Test
     public void getIngredientTypeTest() {
-        Ingredient ingredient =new Ingredient(SAUCE,"test",1000);
+        Ingredient ingredient = new Ingredient(SAUCE, "test", 1000);
         assertEquals(SAUCE, ingredient.getType());
     }
 
