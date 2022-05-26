e882318d2b63d7facce7c1b7eb2794daae127c5e
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-05-26 20:03:25 +0300

    added IngredientTest

---
+++ src/test/java/praktikum/IngredientTest.java
@@ -0,0 +1,55 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class IngredientTest {
+
+    Ingredient ingredient;
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBurgerData(){
+        return new Object[][] {
+                {IngredientType.FILLING, "dinosaur", 200},
+                {IngredientType.SAUCE, "chili sauce", 300},
+        };
+    }
+
+    @Before
+    public void setUp() {
+        ingredient = new Ingredient(type, name, price);
+    }
+
+    @Test
+    public void getPrice() {
+        float actual = ingredient.getPrice();
+        Assert.assertEquals("Ожидаемая цена ингредиента " + price, price, actual, 0);
+    }
+
+    @Test
+    public void getName() {
+        String actual = ingredient.getName();
+        Assert.assertEquals("Ожидаемое название ингредиента - " + name, name, actual);
+    }
+
+    @Test
+    public void getType() {
+        IngredientType actual = ingredient.getType();
+        Assert.assertEquals("Ожидаемый тип ингредиента -" + type, type, actual);
+    }
+}
