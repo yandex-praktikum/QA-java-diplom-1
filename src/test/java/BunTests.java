aef00c6ebe247f0d35641a00c39e133ccbecc657
Nastya Olennikova <olennikovanastyaa@yandex.ru>
2022-05-29 22:35:28 +0300

    BurgerTests added

--- src/test/java/BunTests.java
+++ src/test/java/BunTests.java
@@ -1,35 +1,19 @@
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class BunTests {
-    private final String name;
-    private final float price;
 
-    public BunTests(String name, float price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    @Parameterized.Parameters // добавили аннотацию
-    public static Object[][] getBunData() {
-        return new Object[][]{
-                {"Ржаная", 100},
-                {"Сырная", 99.99f},
-                {"Sesame", 115.35f},
-                {"Классическая", 150}
-        };
+    @Test
+    public void BunNameTest() {
+        Bun bun = new Bun("Классическая", 133.34f);
+        assertEquals("Классическая", bun.getName());
     }
 
     @Test
-    public void isBunDataValid() {
-        Bun bun = new Bun(name, price);
-        assertNotNull(bun.getName());
-        assertTrue(bun.getPrice() > 0);
+    public void BunPriceTest() {
+        Bun bun = new Bun("Классическая", 133.34f);
+        assertEquals(133.34f, bun.getPrice(), 0);
     }
 }
-
--- src/test/java/BurgerTests.java
+++ src/test/java/BurgerTests.java
@@ -1,37 +1,41 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import praktikum.*;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 import static praktikum.IngredientType.FILLING;
-
+import static praktikum.IngredientType.SAUCE;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
 
     @Mock
-    Database database;
-    @Mock
     Bun bun;
-    @Mock
-    Ingredient ingredient;
     @Mock
     Ingredient cheese;
     @Mock
     Ingredient bacon;
     @Mock
-    Ingredient egg;
+    Ingredient garlicSauce;
 
+    @Test
+    public void setBunsTest() {
+        Burger burger = new Burger();
+        Bun bun_rye = new Bun("Ржаная", 140.54f);
+        burger.setBuns(bun_rye);
+        assertEquals(bun_rye, burger.bun);
+    }
 
     @Test
     public void addIngredientTest() {
         Burger burger = new Burger();
-        burger.addIngredient(ingredient);
-        System.out.println(burger.ingredients);
-        assertTrue(burger.ingredients.contains(ingredient));
+        burger.addIngredient(bacon);
+        assertTrue(burger.ingredients.contains(bacon));
     }
 
     @Test
@@ -39,28 +43,25 @@ public class BurgerTests {
         Burger burger = new Burger();
         Ingredient cheese = new Ingredient(FILLING, "CHEESE", 33.45f);
         Ingredient bacon = new Ingredient(FILLING, "BACON", 54.4f);
-        Ingredient egg = new Ingredient(FILLING, "EGG", 12);
+        Ingredient garlicSauce = new Ingredient(SAUCE, "GARLIC SAUCE", 12);
 
         burger.addIngredient(cheese);
         burger.addIngredient(bacon);
-        burger.addIngredient(egg);
+        burger.addIngredient(garlicSauce);
 
         burger.removeIngredient(1);
         assertFalse(burger.ingredients.contains(bacon));
     }
-
     @Test
     public void moveIngredientTest() {
         Burger burger = new Burger();
         Ingredient cheese = new Ingredient(FILLING, "CHEESE", 33.45f);
         Ingredient bacon = new Ingredient(FILLING, "BACON", 54.4f);
-        Ingredient egg = new Ingredient(FILLING, "EGG", 12);
+        Ingredient garlicSauce = new Ingredient(SAUCE, "GARLIC SAUCE", 12);
 
         burger.addIngredient(cheese);
         burger.addIngredient(bacon);
-        burger.addIngredient(egg);
-
-        System.out.println(burger.ingredients);
+        burger.addIngredient(garlicSauce);
         burger.moveIngredient(1, 0);
         assertEquals(burger.ingredients.get(0), bacon);
     }
@@ -69,32 +70,28 @@ public class BurgerTests {
     public void getPriceTest(){
         Burger burger = new Burger();
 
-        Mockito.when(bun.getPrice()).thenReturn(988f);
-        Mockito.when(cheese.getPrice()).thenReturn(33.4f);
-        Mockito.when(bacon.getPrice()).thenReturn(54.6f);
-        Mockito.when(egg.getPrice()).thenReturn(12f);
-
         burger.setBuns(bun);
         burger.addIngredient(bacon);
         burger.addIngredient(cheese);
-        burger.addIngredient(egg);
+        burger.addIngredient(garlicSauce);
 
-        //Может быть, есть какой-то метод, с помощью которого можно сложить несколько чисел с плавающей точкой? К сожалению, мне так и не удалось найти ничего подобного.
-        float ingredientsSum = bun.getPrice()*2 + bacon.getPrice() + cheese.getPrice() + egg.getPrice();
-        assertEquals(burger.getPrice(), ingredientsSum, 0.1);
+        when(bun.getPrice()).thenReturn(988f);
+        when(cheese.getPrice()).thenReturn(33.4f);
+        when(bacon.getPrice()).thenReturn(54.6f);
+        when(garlicSauce.getPrice()).thenReturn(12f);
 
-
+        float ingredientsSum = bun.getPrice()*2 + bacon.getPrice() + cheese.getPrice() + garlicSauce.getPrice();
+        assertEquals(burger.getPrice(), ingredientsSum, 0);
     }
-
     @Test
     public void getReceiptTest() {
-
-
-
-
+        Bun bun_sesame = new Bun ("Кунжутная", 350);
+        Burger burger = new Burger();
+        burger.setBuns(bun_sesame);
+        burger.addIngredient(new Ingredient(FILLING, "bacon", 120));
+        burger.addIngredient(new Ingredient(SAUCE, "garlic sauce", 150));
+        String burger_receipt = "(==== Кунжутная ====)" + "\n" + "= filling bacon =" + "\n" + "= sauce garlic sauce =" + "\n" + "(==== Кунжутная ====)" + "\n" + "\n" + "Price: 970.000000" + "\n";
+        assertEquals(burger_receipt, burger.getReceipt());
     }
-
-
-
 
 }
