f2c2fc337a7943e98f82fe3cb4d086c7986fa586
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2022-06-13 02:43:25 +0300

    add unit tests

--- src/test/java/TestBurger.java
+++ src/test/java/TestBurger.java
@@ -15,11 +15,41 @@ public class TestBurger {
     Bun bun;
     @Mock
     Ingredient ingredient;
+    @Mock
+    Ingredient newIngredient;
     Burger burger = new Burger();
 
     @Test
     public void checkSetBun() {
         burger.setBuns(bun);
-        Assert.assertTrue(burger.bun != null);
+        Assert.assertNotNull(burger.bun);
+    }
+
+    @Test
+    public void checkAddIngridient() {
+        int initialSize = burger.ingredients.size();
+        burger.addIngredient(ingredient);
+        Assert.assertEquals("Не добавлен ингридиент бургера", 1, burger.ingredients.size() - initialSize);
+    }
+
+    @Test
+    public void checkRemoveIngridient() {
+        int initialSize = burger.ingredients.size();
+        burger.addIngredient(ingredient);
+        burger.removeIngredient(burger.ingredients.size() - 1);
+        Assert.assertEquals("Не удалён ингридиент бургера", initialSize, burger.ingredients.size());
+    }
+
+    @Test
+    public void checkMoveIngridient() {
+        String newIngredientName = "Тест";
+        burger.addIngredient(ingredient);
+        burger.addIngredient(newIngredient);
+        int currentSize = burger.ingredients.size();
+        int index = currentSize - 1;
+        int newIndex = currentSize - 2;
+        burger.moveIngredient(index, newIndex);
+        Mockito.when(newIngredient.getName()).thenReturn(newIngredientName);
+        Assert.assertEquals("Не добавлен ингридиент бургера", newIngredientName, burger.ingredients.get(newIndex).getName());
     }
 }
