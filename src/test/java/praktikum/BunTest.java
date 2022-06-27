4272ef46ccff92117c9c26b9c089ac7d71f331e5
Diana Garifullina <hinsaksas@gmail.com>
2022-06-28 03:39:16 +0500

    first version

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,29 @@
+package praktikum;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class BunTest {
+
+    @Test
+    public void testGetName() {
+        Faker faker = new Faker();
+        String name = faker.name().firstName();
+        float price = faker.number().randomNumber();
+        Bun bun = new Bun(name, price);
+        System.out.println(bun.getName());
+        Assert.assertEquals(name, bun.getName());
+    }
+
+    @Test
+    public void testGetPrice() {
+        Faker faker = new Faker();
+        String name = faker.name().firstName();
+        float price = faker.number().randomNumber();
+        Bun bun = new Bun(name, price);
+        System.out.println(bun.getPrice());
+        Assert.assertEquals(price, bun.getPrice(), 0);
+    }
+
+}
---
+++ src/test/java/praktikum/IngredientParamTest.java
@@ -0,0 +1,52 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class IngredientParamTest {
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientParamTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[] ingredients(){
+        return new Object[][]{
+                {IngredientType.SAUCE, "with sauce", 50},
+                {IngredientType.FILLING, "with filling", 99},
+        };
+    }
+
+    @Test
+    public void testGetPrice() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        System.out.println(ingredient);
+        System.out.println(ingredient.getPrice());
+        Assert.assertEquals(price, ingredient.getPrice(), 0);
+    }
+
+    @Test
+    public void testGetName() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        System.out.println(ingredient);
+        System.out.println(ingredient.getName());
+        Assert.assertEquals(name, ingredient.getName());
+    }
+
+    @Test
+    public void testGetType() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        System.out.println(ingredient);
+        System.out.println(ingredient.getType());
+        Assert.assertEquals(type, ingredient.getType());
+    }
+
+}
