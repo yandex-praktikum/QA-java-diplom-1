d3dc3377e1f3ace24eae880394a7c3a5c0c3495d
Tanya Aleksandrova <tan-aleksandrova2102@yandex.ru>
2022-09-01 20:09:53 +0300

    добавленs тесты с использованием класса Database

--- src/test/java/praktikum/bun/BunTest.java
+++ src/test/java/praktikum/bun/BunTest.java
@@ -1,12 +1,24 @@
 package praktikum.bun;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 import praktikum.Bun;
+import praktikum.Database;
 
 public class BunTest {
 
+    @Mock
+    Database database;
+
     @Test public void getNameTest(){
         Bun bun = new Bun("Булка1", (float) 56.6);
+        String actual = bun.getName();
+        Assert.assertEquals(bun.name,actual);
+    }
+
+    @Test public void getNameTestFromDB(){
+        Database database = new Database();
+        Bun bun = database.availableBuns().get(1);
         String actual = bun.getName();
         Assert.assertEquals(bun.name,actual);
     }
@@ -16,4 +28,13 @@ public class BunTest {
         float actual = bun.getPrice();
         Assert.assertEquals(bun.price,actual,0);
     }
+
+    @Test public void getPriceTestFromDB(){
+        Database database = new Database();
+        Bun bun = database.availableBuns().get(1);
+        float actual = bun.getPrice();
+        Assert.assertEquals(bun.price,actual,0);
+    }
+
+
 }
--- src/test/java/praktikum/ingredient/IngredientTest.java
+++ src/test/java/praktikum/ingredient/IngredientTest.java
@@ -2,6 +2,7 @@ package praktikum.ingredient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.*;
 
@@ -10,11 +11,23 @@ import static org.junit.Assert.*;
 @RunWith(MockitoJUnitRunner.class)
 public class IngredientTest {
 
+    @Mock
+    Database database;
+
     @Test
     public void getPrice() {
         Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"Сырный", 20);
         float actual = ingredient.getPrice();
         float expected = 20;
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void getPriceFromDB() {
+        Database database = new Database();
+        Ingredient ingredient = database.availableIngredients().get(1);
+        float actual = ingredient.getPrice();
+        float expected = database.availableIngredients().get(1).price;
         assertEquals(expected, actual, 0);
     }
 
@@ -27,9 +40,26 @@ public class IngredientTest {
     }
 
     @Test
+    public void getNameFromDb() {
+        Database database = new Database();
+        Ingredient ingredient = database.availableIngredients().get(2);
+        String actual = ingredient.getName();
+        String expected = database.availableIngredients().get(2).name;
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void getType() {
         Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"Кетчуп", 15);
         IngredientType actual = ingredient.getType();
+        IngredientType expected = IngredientType.SAUCE;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTypeFromDB() {
+        Database database = new Database();
+        IngredientType actual = database.availableIngredients().get(1).getType();
         IngredientType expected = IngredientType.SAUCE;
         assertEquals(expected, actual);
     }
