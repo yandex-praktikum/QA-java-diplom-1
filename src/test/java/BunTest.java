e5bb3e43513325e38486db7b815394a45842aaec
Vyacheslav Bryzgachev <slavavbryzgachev@yandex.ru>
2022-09-06 18:38:02 +0300

    FirstCommit

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BunTest {
+    private Bun bun;
+
+    @Before
+    public void createNewInstance() {
+        bun = new Bun("original", 200.0f);
+    }
+
+    @Test
+    public void getName() {
+        String expected = "original";
+        String actual = bun.getName();
+
+        assertEquals("Incorrect values bun name", expected, actual);
+    }
+
+    @Test
+    public void getPrice() {
+        float expected = 200.0f;
+        float actual = bun.getPrice();
+
+        assertEquals("Incorrect values bun price", expected, actual, 0);
+    }
+}
