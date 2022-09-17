cf51e11e1bcfd64168f798393d8d29e33f347f5b
IvanFitiskin <ifitiskin93@gmail.com>
2022-09-17 15:01:01 +0300

    added test for enum class

---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,18 @@
+package praktikum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTypeTest {
+
+    @Test
+    public void values() {
+        IngredientType[] ingredientTypesValues = IngredientType.values();
+
+        assertEquals(
+                "Количество ингредиентов отличается от ожидаемого",
+                2,
+                ingredientTypesValues.length);
+    }
+}
