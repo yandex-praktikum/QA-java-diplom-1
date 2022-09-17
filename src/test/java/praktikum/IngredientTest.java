c4a9e35e6a301b7ac05e282e57899a74f3e99948
IvanFitiskin <ifitiskin93@gmail.com>
2022-09-17 15:22:40 +0300

    added test for Ingredient class

---
+++ src/test/java/praktikum/IngredientTest.java
@@ -0,0 +1,49 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class IngredientTest {
+
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Делаем параметризацию данных на случай использования констант в методах getName() и getPrice()
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][] {
+                { IngredientType.SAUCE, "Соус Spicy-X", 90 },
+                { IngredientType.FILLING, "Мясо бессмертных моллюсков Protostomia", 1337 },
+        };
+    }
+
+    @Test
+    public void getPrice() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(price, ingredient.getPrice(), 0.0f);
+    }
+
+    @Test
+    public void getName() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(name, ingredient.getName());
+    }
+
+    @Test
+    public void getType() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(type, ingredient.getType());
+    }
+}
