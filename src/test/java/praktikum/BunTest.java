c0ba91bc210c28deaa9c34e507b6f4a64a0d0804
Abramovich Andrey <aabramovich@usetech.ru>
2022-09-20 19:12:18 +0300

    update bun and ingredient tests to parameterized

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,27 +1,47 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    private final String name = "Space bun";
-    private final float price = 50F;
+    private final String name;
+    private final float price;
+    private final Bun bun;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+        this.bun = new Bun(name, price);
+    }
+
+    @Parameterized.Parameters(name = "Test data: {0}")
+    public static Collection<Object[]> getTestData() {
+        return Arrays.asList(new Object[][] {
+                {"black bun", 100},
+                {"white bun", 200},
+                {"red bun", 300},
+                {"", 1000000000F},
+                {"redbuntestisverylongstringforparamterizedtest", -1},
+                {null, 0}
+        });
+    }
+
 
     @Test
     public void getNameReturnsBunName() {
-        Bun bun = new Bun(name, price);
-        String actualResult = bun.getName();
-
-        assertEquals("The name is not 'Space bun'", name, actualResult);
+        assertEquals(name, bun.getName());
     }
 
     @Test
     public void getPriceReturnsBunPrice() {
-        Bun bun = new Bun(name, price);
-        float actualResult = bun.getPrice();
-
-        assertEquals(price, actualResult, 0.00001);
+        assertEquals(price, bun.getPrice(), 0.01);
     }
 }
--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -2,37 +2,61 @@ package praktikum;
 
 import net.bytebuddy.description.field.FieldDescription;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
 
-    private final IngredientType type = IngredientType.SAUCE;
-    private final String name = "Space ingredient";
-    private final float price = 10F;
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+    private Ingredient ingredient;
+
+    public IngredientTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+        this.ingredient = new Ingredient(type, name, price);
+    }
+
+    @Parameterized.Parameters(name = "Test data: {0}")
+    public static Collection<Object[]> getTestData() {
+        return Arrays.asList(new Object[][] {
+                {IngredientType.SAUCE, "hot sauce", 100},
+                {IngredientType.SAUCE, "sour cream", 200},
+                {IngredientType.SAUCE, "chili sauce", 300},
+                {IngredientType.SAUCE, "", 1000000000F},
+                {IngredientType.FILLING, "cutlet", 100},
+                {IngredientType.FILLING, "dinosaur", 200},
+                {IngredientType.FILLING, "sausage", 300},
+                {IngredientType.FILLING, "hotsauceissohotforthisproject", -1},
+                {IngredientType.FILLING, null, 0}
+
+        });
+    }
 
     @Test
     public void getPriceReturnsPrice() {
         Ingredient ingredient = new Ingredient(type, name, price);
-        float actualResult = ingredient.getPrice();
-
-        assertEquals(price, actualResult, 0.00001);
+        assertEquals(price,ingredient.getPrice(), 0.00001);
     }
 
     @Test
     public void getNameReturnsName() {
-        Ingredient ingredient = new Ingredient(type, name, price);
-        String actualResult = ingredient.getName();
 
-        assertEquals("The ingredient name is not equals", name, actualResult);
+        assertEquals("The ingredient name is not equals", name, ingredient.getName());
     }
 
     @Test
     public void getTypeReturnsType() {
-        Ingredient ingredient = new Ingredient(type, name, price);
-        IngredientType actualResult = ingredient.getType();
 
-        assertEquals(type, actualResult);
+        assertEquals(type, ingredient.getType());
     }
 
 }
