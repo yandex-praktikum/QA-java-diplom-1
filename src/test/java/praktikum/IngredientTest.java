8fa2515617f864f73fc235f16a212c5755c4ce30
Valeriia Kurnosova <valeriyak@yandex.ru>
2022-09-27 18:09:59 +0300

    Update after review

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
@@ -25,8 +27,68 @@ public class IngredientTest {
     }
 
     @Test
-    public void getNameReturnsCorrectValues() {
+    public void getNameReturnsCorrectValuesOnlyLetters() {
         String expectedName = faker.regexify("[A-Za-z]{6}");
+        float expectedPrice = faker.number().numberBetween(1,10000);
+        IngredientType expectedType = IngredientType.SAUCE;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        String actualName = ingredient.getName();
+        assertEquals("Name is not correct: ", expectedName, actualName);
+    }
+
+    @Test
+    public void getNameReturnsCorrectValuesOnlyDigits() {
+        String expectedName = faker.regexify("[0-9]{6}");
+        float expectedPrice = faker.number().numberBetween(1,10000);
+        IngredientType expectedType = IngredientType.SAUCE;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        String actualName = ingredient.getName();
+        assertEquals("Name is not correct: ", expectedName, actualName);
+    }
+
+    @Test
+    public void getNameReturnsCorrectValuesSpecialCharacters() {
+        String expectedName = faker.internet().password(6,20,true, true);
+        float expectedPrice = faker.number().numberBetween(1,10000);
+        IngredientType expectedType = IngredientType.SAUCE;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        String actualName = ingredient.getName();
+        assertEquals("Name is not correct: ", expectedName, actualName);
+    }
+
+    @Test
+    public void getNameReturnsCorrectValuesEmptyName() {
+        String expectedName = "";
+        float expectedPrice = faker.number().numberBetween(1,10000);
+        IngredientType expectedType = IngredientType.SAUCE;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        String actualName = ingredient.getName();
+        assertEquals("Name is not correct: ", expectedName, actualName);
+    }
+
+    @Test
+    public void getNameReturnsCorrectValuesNullName() {
+        String expectedName = null;
+        float expectedPrice = faker.number().numberBetween(1,10000);
+        IngredientType expectedType = IngredientType.SAUCE;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        String actualName = ingredient.getName();
+        assertEquals("Name is not correct: ", expectedName, actualName);
+    }
+
+    @Test
+    public void getNameReturnsCorrectValuesLongName() {
+        String expectedName = faker.regexify("[A-Za-z]{100}");
         float expectedPrice = faker.number().numberBetween(1,10000);
         IngredientType expectedType = IngredientType.SAUCE;
 
@@ -40,6 +102,55 @@ public class IngredientTest {
     public void getPriceReturnsCorrectValues() {
         String expectedName = faker.regexify("[A-Za-z]{6}");
         float expectedPrice = faker.number().numberBetween(1,10000);
+        IngredientType expectedType = IngredientType.FILLING;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        float actualPrice = ingredient.getPrice();
+        assertEquals("Price is not correct: ", expectedPrice, actualPrice, 0.1);
+    }
+
+    @Test
+    public void getPriceReturnsCorrectLowPrice() {
+        Random r = new Random();
+        String expectedName = faker.regexify("[A-Za-z]{6}");
+        float expectedPrice = (float) (0.0001 + r.nextFloat() * (0.01 - 0.0001));
+        IngredientType expectedType = IngredientType.FILLING;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        float actualPrice = ingredient.getPrice();
+        assertEquals("Price is not correct: ", expectedPrice, actualPrice, 0.1);
+    }
+
+    @Test
+    public void getPriceReturnsCorrectHighPrice() {
+        String expectedName = faker.regexify("[A-Za-z]{6}");
+        float expectedPrice = faker.number().numberBetween(1000000,1000000000);
+        IngredientType expectedType = IngredientType.FILLING;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        float actualPrice = ingredient.getPrice();
+        assertEquals("Price is not correct: ", expectedPrice, actualPrice, 0.1);
+    }
+
+    @Test
+    public void getPriceReturnsCorrectZeroPrice() {
+        String expectedName = faker.regexify("[A-Za-z]{6}");
+        float expectedPrice = 0;
+        IngredientType expectedType = IngredientType.FILLING;
+
+        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+
+        float actualPrice = ingredient.getPrice();
+        assertEquals("Price is not correct: ", expectedPrice, actualPrice, 0.1);
+    }
+
+    @Test
+    public void getPriceReturnsCorrectUnderZeroPrice() {
+        String expectedName = faker.regexify("[A-Za-z]{6}");
+        float expectedPrice = faker.number().numberBetween(-10000, -1);
         IngredientType expectedType = IngredientType.FILLING;
 
         Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
