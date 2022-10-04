878979df9cba8602935ec4a0e8a987d519a47c46
Mihail Seregin <mihail.mieke@yandex.ru>
2022-10-04 09:22:53 +0300

    create all tests and add jacoco report

---
+++ src/main/java/praktikum/ExpectedReceiptsConstant.java
@@ -0,0 +1,11 @@
+package praktikum;
+
+public class ExpectedReceiptsConstant {
+    public static final String EXPECTED_RECEIPT = "(==== black bun ====)\r\n" +
+            "= sauce sour cream =\r\n" +
+            "= filling cutlet =\r\n" +
+            "= filling dinosaur =\r\n" +
+            "(==== black bun ====)\r\n" +
+            "\r\n" +
+            "Price: 700,000000\r\n";
+}
