f9fbf537eb572eb51f9e1776eafb4e87e2728d2d
Aleksey Tester <akolyaev@bk.ru>
2022-10-12 23:05:03 -0700

    Fix BurgerIngredientTest

--- src/test/java/praktikum/BurgerIngredientTest.java
+++ src/test/java/praktikum/BurgerIngredientTest.java
@@ -1,80 +1,74 @@
 package praktikum;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-
+import java.util.List;
 import java.util.Random;
 
-import static org.hamcrest.CoreMatchers.endsWith;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
-@RunWith(MockitoJUnitRunner.class)
 public class BurgerIngredientTest {
 
-    @Mock
-    private Ingredient ingredient;
-    @Mock
-    private Ingredient anotherIngredient;
-    @Mock
-    private Burger burger;
-    @Mock
-    private Bun bun;
+    Database database = new Database();
+    Burger burger;
+    List<Bun> buns = database.availableBuns();
+    List<Ingredient> ingredients = database.availableIngredients();
+    Bun bun;
+    Ingredient ingredient;
+    Ingredient anotherIngredient;
+
+    @Before
+    public void createBurger() {
+        int bunIndex = new Random().nextInt(buns.size());
+        int ingredientIndex = new Random().nextInt(ingredients.size());
+        this.burger = new Burger();
+        this.ingredient = ingredients.get(ingredientIndex);
+        this.bun = buns.get(bunIndex);
+        burger.setBuns(bun);
+    }
 
     @Test
     public void checkAddIngredient() {
         burger.addIngredient(ingredient);
-        Mockito.verify(burger).addIngredient(ingredient);
+        assertThat(burger.getReceipt(), containsString(ingredient.getName()));
     }
 
     @Test
     public void checkRemoveIngredient() {
-        burger.setBuns(bun);
         burger.addIngredient(ingredient);
         burger.removeIngredient(0);
-        Mockito.verify(burger).removeIngredient(0);
+        assertThat(burger.getReceipt(), not(containsString((ingredient.getName()))));
     }
 
     @Test
     public void checkMoveIngredient() {
-        burger.setBuns(bun);
+        this.anotherIngredient = ingredients.get(new Random().nextInt(ingredients.size()));
         burger.addIngredient(ingredient);
         burger.addIngredient(anotherIngredient);
         burger.moveIngredient(0,1);
-        Mockito.verify(burger).moveIngredient(0, 1);
+        assertThat(burger.getReceipt(), containsString(String.format("%s =%n(", ingredient.getName())));
     }
 
     @Test
-    public void checkGetPriceBunsOnlyTest() {
-        float randomPrice = new Random().nextFloat();
-        this.bun = new Bun("", randomPrice);
-        this.burger = new Burger();
-        burger.setBuns(bun);
-        assertEquals((randomPrice * 2), burger.getPrice(), 0);
-    }
-
-    @Test
-    public void checkGetPriceBunsAndIngredientTest() {
-        float randomPrice = new Random().nextFloat();
-        this.bun = new Bun("", randomPrice);
-        this.burger = new Burger();
-        this.ingredient = new Ingredient(IngredientType.SAUCE, "", randomPrice);
-        burger.setBuns(bun);
+    public void checkGetPriceWithIngredient() {
         burger.addIngredient(ingredient);
-        assertEquals((randomPrice * 3), burger.getPrice(), 0);
+        assertEquals(bun.getPrice() *2 + ingredient.getPrice(), burger.getPrice(), 0);
     }
 
     @Test
-    public void checkGetReceiptTest() {
-        float randomPrice = new Random().nextFloat();
-        this.bun = new Bun("KolyaevBun", randomPrice);
-        this.burger = new Burger();
-        this.ingredient = new Ingredient(IngredientType.SAUCE, "KolyaevIngredient", randomPrice);
-        burger.setBuns(bun);
+    public void checkGetPriceWithoutIngredient() {
+        assertEquals(bun.getPrice() *2, burger.getPrice(), 0);
+    }
+
+    @Test
+    public void checkGetReceiptWithIngredient() {
         burger.addIngredient(ingredient);
-        Assert.assertThat(burger.getReceipt(), endsWith(String.format("%.6f%n", (randomPrice * 3))));
+        assertThat(burger.getReceipt(), endsWith(String.format("%.6f%n", bun.getPrice() * 2 + ingredient.getPrice())));
+    }
+
+    @Test
+    public void checkGetReceiptWithoutIngredient() {
+        assertThat(burger.getReceipt(), endsWith(String.format("%.6f%n", bun.getPrice() * 2)));
     }
 }
