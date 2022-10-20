fd6f1f45905ada75a5cfc726689ec79f69b754e4
Alina Gainetdinova <malvinaalina@yandex.ru>
2022-10-20 21:37:15 +0500

    fix tests

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -4,37 +4,40 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 @RunWith(Parameterized.class)
 public class BunTest {
 
-    public String name;
-    public float price;
+    private final String name;
+    private final float price;
 
-    public void BunTest(String name, float price) {
+    public BunTest(String name, float price) {
         this.name = name;
         this.price = price;
     }
 
     @Parameterized.Parameters
-    public static Object[][] getParameters() {
-        return new Object[][]{
-                {"black bun", 100},
-                {"white bun", 200},
-                {"red bun", 300},
-        };
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"black bun", 100.0f},
+                {"white bun", 200.0f},
+                {"red bun", 300.0f},
+        });
     }
-        @Test
-        public void getNameTest() {
-            Bun bun = new Bun(name, price);
-            String name = bun.getName();
-            Assert.assertEquals(name, "black bun");
-        }
-;
-        @Test
-        public void getPriceTest() {
-            Bun bun = new Bun("black bun", 100);
-            float price = bun.getPrice();
-            Assert.assertEquals(price, 100, 0.0001);
-        }
+    @Test
+    public void getNameTest() {
+        Bun bun = new Bun(name, price);
+        String actualName = bun.getName();
+        Assert.assertEquals(actualName, name);
     }
+
+    @Test
+    public void getPriceTest() {
+        Bun bun = new Bun(name, price);
+        float actualPrice = bun.getPrice();
+        Assert.assertEquals(actualPrice, price, 0.0001);
+    }
+}
 
