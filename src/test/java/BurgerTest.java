6fc9303275c4ccf731c903e9aae3bd100136dbb0
Oleg <ripperbnet@yandex.ru>
2022-11-09 22:26:33 +0300

    added test

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,45 +1,42 @@
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
-import praktikum.Database;
 import praktikum.Ingredient;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+import static praktikum.IngredientType.SAUCE;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
 
 
 
-    @Mock
-    private Database database;
 
-  //  @Mock
-  //  private Bun bun;
+
+    @Mock
+    private Bun bun;
+
+    @Mock
+    private Ingredient ingredient;
+
+    @Mock
+    private Burger burger;
 
 
     @Test
     public void getPriceTest() {
-   //     Mockito.when(database.availableBuns()).thenReturn("black bun", 100");
-   //     Mockito.when(bun.getPrice()).thenReturn(20F);
+        Mockito.when(bun.getPrice()).thenReturn(200f);
+        Mockito.when(ingredient.getPrice()).thenReturn(300f);
         Burger burger = new Burger();
-        Database database = new Database();
-        List<Ingredient> ingredients = database.availableIngredients();
-        List<Bun> buns = database.availableBuns();
-        burger.setBuns(buns.get(0));
-        burger.addIngredient(ingredients.get(1));
-        int actual = (int) burger.getPrice();
-        int expected = 400;
-        Assert.assertEquals(expected, actual);
-
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        float expected = 700f;
+        assertEquals(expected, burger.getPrice(), 0.0f);
     }
+
 }
