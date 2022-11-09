5a8f98f8e9895667cd2ef5ef05abafb185781b46
Oleg <ripperbnet@yandex.ru>
2022-11-09 22:58:52 +0300

    added second test

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -5,10 +5,14 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
+import praktikum.Database;
 import praktikum.Ingredient;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
+import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -39,4 +43,21 @@ public class BurgerTest {
         assertEquals(expected, burger.getPrice(), 0.0f);
     }
 
+    @Test
+    public void getReceiptTest() {
+        Mockito.when(ingredient.getName()).thenReturn("cutlet");
+        Mockito.when(bun.getName()).thenReturn("black bun");
+        Mockito.when(bun.getPrice()).thenReturn(50f);
+        Mockito.when(ingredient.getPrice()).thenReturn(100f);
+        Mockito.when(ingredient.getType()).thenReturn(FILLING);
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        String expected = "(==== black bun ====)\r\n" +
+                          "= filling cutlet =\r\n" +
+                          "(==== black bun ====)\r\n" +
+                          "\r\n" +
+                          "Price: 200,000000\r\n";
+        assertEquals(expected, burger.getReceipt());
+    }
 }
