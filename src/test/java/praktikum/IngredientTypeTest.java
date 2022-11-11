0dbdc8687177aa63dd28ec4bac1cd6645f5f88a1
InnaGG <innageldyeva@mail.ru>
2022-11-11 10:52:30 +0100

    added IngredientType class test

---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,15 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IngredientTypeTest {
+    public IngredientTypeTest() {
+    }
+
+    @Test
+    public void checkIngredients() {
+        Assert.assertEquals("SAUCE", String.valueOf(IngredientType.SAUCE));
+        Assert.assertEquals("FILLING", String.valueOf(IngredientType.FILLING));
+    }
+}
