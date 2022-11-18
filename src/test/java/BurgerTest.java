6f112e55beda9c56642757c9e1f680ce1274d01f
dgolubinskiy <dgolubinskiy@iiko.ru>
2022-11-19 01:19:18 +0300

    Added BunTest and IngredientTypeTest.

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,79 +1,75 @@
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import praktikum.Ingredient;
-//import praktikum.IngredientType;
-//
-//
-//import java.util.List;
-//
-//import static org.hamcrest.CoreMatchers.equalTo;
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.junit.Assert.assertEquals;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//
-//public class BurgerTest {
-//    @Mock
-//    Bun bun;
-//    @Mock
-//    Ingredient ingredient;
-//    @Mock
-//    List<Ingredient> ingredients;
-//
-//    /**
-//     * Проверка, что метод добавляет в бургер разные ингредиенты: соусы и начинки
-//     */
-//    @Test
-//    public void addSomeIngredientTest(){
-//
-//    }
-//
-//    /**
-//     * Проверка, что метод удаляет ингредиент из булочки
-//     */
-//    @Test
-//    public void removeSomeIngredientTest() {
-//
-//    }
-//
-//    /**
-//     * Проверка, что метод может перемещать (менять местами) ингредиенты внутри булочки
-//     */
-//    @Test
-//    public void moveSomeIngredientsTest() {
-//
-//    }
-//
-//    /**
-//     * Проверка, что метод возвращает цену бургера
-//     */
-//    @Test
-//    public void getBurgerPriceTest() {
-//        float price = 600F;
-//        Mockito.when(bun.getPrice()).thenReturn(300F);
-//        Mockito.when(ingredient.getPrice()).thenReturn(300F);
-//        Burger burger = new Burger();
-//        Burger.setBuns(bun);
-//        Burger.setIngredient(ingredient);
-//        assertThat(price, equalTo(burger.getPrice()));
-//    }
-//
-//    /**
-//     * Проверка, что метод возвращает чек с информацией о бургере
-//     */
-//    @Test
-//    public void getReceiptTest() {
-//        Mockito.when(bun.getName()).thenReturn("red bun");
-//        Burger.setBuns(bun);
-//        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
-//        Mockito.when(ingredient.getName()).thenReturn("sausage");
-//        Mockito.when(bun.getPrice()).thenReturn(300F);
-//        Mockito.when(ingredient.getPrice()).thenReturn(300F);
-//        Burger burger = new Burger();
-//        assertEquals(true, burger.getReceipt().contains("red bun")); // todo: проверить формат
-//
-//    }
-//}
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class BurgerTest {
+    float expectedPrice = 600F;
+    @Mock
+    Bun bun;
+    @Mock
+    Ingredient ingredient;
+
+    /**
+     * Проверка, что метод добавляет в бургер разные ингредиенты: соусы и начинки
+     */
+    @Test
+    public void addSomeIngredientTest(){
+
+    }
+
+    /**
+     * Проверка, что метод удаляет ингредиент из булочки
+     */
+    @Test
+    public void removeSomeIngredientTest() {
+
+    }
+
+    /**
+     * Проверка, что метод может перемещать (менять местами) ингредиенты внутри булочки
+     */
+    @Test
+    public void moveSomeIngredientsTest() {
+
+    }
+
+    /**
+     * Проверка, что метод возвращает цену бургера
+     */
+    @Test
+    public void getBurgerPriceTest() {
+        Burger burger = new Burger(bun);
+        Mockito.when(bun.getPrice()).thenReturn(300F);
+        Mockito.when(ingredient.getPrice()).thenReturn(300F);
+        assertThat(expectedPrice, equalTo(burger.getPrice()));
+    }
+
+    /**
+     * Проверка, что метод возвращает чек с информацией о бургере
+     */
+    @Test
+    public void getReceiptTest() {
+        Mockito.when(bun.getName()).thenReturn("red bun");
+        Burger.setBuns(bun);
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(ingredient.getName()).thenReturn("sausage");
+        Mockito.when(bun.getPrice()).thenReturn(300F);
+        Mockito.when(ingredient.getPrice()).thenReturn(300F);
+        Burger burger = new Burger();
+        assertEquals(true, burger.getReceipt().contains("red bun")); // todo: проверить формат
+
+    }
+}
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,22 +1,52 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
 import praktikum.IngredientType;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
+    private final IngredientType ingredientType;
+    String ingredientName;
+    float ingredientPrice;
+
+    public IngredientTest(IngredientType ingredientType, String ingredientName, float ingredientPrice) {
+        this.ingredientType = ingredientType;
+        this.ingredientName = ingredientName;
+        this.ingredientPrice = ingredientPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] allIngredients() {
+        return new Object[][] {
+                {IngredientType.SAUCE, "hot sauce", 100F},
+                {IngredientType.SAUCE, "sour cream", 200F},
+                {IngredientType.SAUCE, "chili sauce", 300F},
+                {IngredientType.FILLING, "cutlet", 100F},
+                {IngredientType.FILLING, "dinosaur", 200F},
+                {IngredientType.FILLING, "sausage", 300F},
+        };
+    }
 
     /**
      * Проверка, что метод возвращает цену ингредиента
      */
     @Test
-    public void getPriceSomeIngredientTest() { // todo: параметризация
-
+    public void getPriceSomeIngredientTest() {
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        float actual = ingredient.getPrice();
+        Assert.assertEquals(ingredientPrice, actual, 0.05);
     }
 
     /**
      * Проверка, что метод возвращает название ингредиента
      */
     @Test
-    public void getNameSomeIngredientTest() { // todo: параметризация
-
+    public void getNameSomeIngredientTest() {
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        String actual = ingredient.getName();
+        Assert.assertEquals(ingredientName, actual);
     }
 
     /**
@@ -24,6 +54,8 @@ public class IngredientTest {
      */
     @Test
     public void getTypeSomeIngredientTest() {
-
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        IngredientType actual = ingredient.getType();
+        Assert.assertEquals(ingredientType, actual);
     }
 }
