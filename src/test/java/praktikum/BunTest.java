b0a16489d4581335279b36493507d3847765e8ff
artem-kot <artem.alex.kot@gmail.com>
2022-12-01 22:35:37 +0100

    Added test structure and dependencies in pom.

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,16 @@
+package praktikum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BunTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getPrice() {
+    }
+}
