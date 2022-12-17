85b31d1d37cf47530fa51dbe35ecb2aab2b57936
Popsinator <pop.alekseika@yandex.ru>
2022-12-17 17:57:39 +0300

    fix: поправил доп. замечания к Diplom_1

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,40 +1,40 @@
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.MockitoAnnotations;
 import praktikum.Bun;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
 public class BunTest {
 
     private final String name;
     private final float price;
-    private final Bun expectBun;
+    private final String expectName;
+    private final float expectPrice;
 
-    public BunTest(String name, float price, Bun expectBun) {
+
+    public BunTest(String name, float price, String expectName, float expectPrice) {
         this.name = name;
         this.price = price;
-        this.expectBun = expectBun;
+        this.expectName = expectName;
+        this.expectPrice = expectPrice;
     }
 
     @Parameterized.Parameters
     public static Object[][] testData() {
         return new Object[][] {
-                {"Test", 100500, new Bun("Test", 100500)},
-                {null, 100500, new Bun(null, 100500)},
-                {"Test", 0, new Bun("Test", 0)},
-                {"Test", -1, new Bun("Test", -1)}
+                {"Test", 100500, "Test", 100500},
+                {null, 100500, null, 100500},
+                {"Test", 0, "Test", 0},
+                {"Test", -1, "Test", -1}
         };
     }
 
     @Test
     public void bunCreateTest() {
         Bun actualBun = new Bun(name, price);
-        assertEquals(expectBun.getName(), actualBun.getName());
-        assertEquals(true, Math.abs(expectBun.getPrice() - actualBun.getPrice()) < 0.00001);
+        assertEquals(expectName, actualBun.getName());
+        assertEquals(expectPrice, actualBun.getPrice(), 0.0001);
     }
 }
