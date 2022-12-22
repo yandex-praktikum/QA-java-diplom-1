13495ece9af0be9748be6a98cc700c0aaee85b5a
Evgeny Pichugin <epichugin@openintegration.inc>
2022-12-22 17:11:44 +0300

    made IngredientTest parameterized

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,30 +1,50 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
 
 	private Ingredient ingredient;
+	private String ingredientType;
+	String name;
+	float price;
+
+	public IngredientTest(String ingredientType, String name, float price) {
+		this.ingredientType = ingredientType;
+		this.name = name;
+		this.price = price;
+	}
+
+	@Parameterized.Parameters(name = "Ingredient Type = {0}, Name = {1}, Price = {2}")
+	public static Object[][] getData() {
+		return new Object[][]{
+				{"SAUCE", "Круассан", 66.66f},
+				{"FILLING", "Рогалик", 71.71f},
+		};
+	}
 
 	@Before
 	public void setUp(){
-		ingredient = new Ingredient(IngredientType.SAUCE, "Рогалик", 66.66f);
+		ingredient = new Ingredient(IngredientType.valueOf(ingredientType), name, price);
 	}
 
 	@Test
 	public void getPriceTest(){
-		Assert.assertEquals(66.66f, ingredient.getPrice(), 0);
+		Assert.assertEquals(price, ingredient.getPrice(), 10);
 	}
 
 	@Test
 	public void getNameTest(){
-		Assert.assertEquals("Рогалик", ingredient.getName());
+		Assert.assertEquals(name, ingredient.getName());
 	}
 
 	@Test
 	public void getTypeTest(){
-		Assert.assertEquals(IngredientType.SAUCE, ingredient.getType());
+		Assert.assertEquals(IngredientType.valueOf(ingredientType), ingredient.getType());
 	}
 }
