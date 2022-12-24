6d569608a25d8b500712a0334e438dc4d29b0e78
Elena Fomchenkova <lenafom4enkova@yandex.ru>
2022-12-24 17:22:59 +0300

    changed bun tests to parametrized bun tests

--- src/test/java/BunParametrizedTest.java
+++ src/test/java/BunParametrizedTest.java
@@ -9,17 +9,45 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 public class BunParametrizedTest {
 
+private String name;
+private float price;
+private String expectedName;
+private float expectedPrice;
 
+public BunParametrizedTest(String name, float price,
+                           String expectedName, float expectedPrice) {
+    this.name = name;
+    this.price = price;
+    this.expectedName = expectedName;
+    this.expectedPrice = expectedPrice;
+}
+
+    @Parameterized.Parameters
+    public static Object [][] getTestData(){
+        return new Object[][]{
+                {"Булочка", 45f, "Булочка", 45f},
+                {"", 45f, "", 45f},
+                {"Очень длинное название булочки, которое будет невозможно напечатать ни на одном чеке разумной величины",
+                45f, "Очень длинное название булочки, которое будет невозможно напечатать ни на одном чеке разумной величины", 45f},
+                {null, 45f, null, 45f},
+                {"*&^^%$##@##$#^&^&**!.,/*-+:;'", 45f, "*&^^%$##@##$#^&^&**!.,/*-+:;'", 45f},
+                {"Булочка", -55f,"Булочка", -55f},
+                {"Булочка", 0f, "Булочка", 0f},
+                {"Булочка", 0.00000001f,
+                        "Булочка", 0.00000001f},
+                {"Булочка", -6546554654654646445465f, "Булочка", -6546554654654646445465f},
+        };
+    }
 
  @Test
 public void getNameTest(){
-     Bun bun = new Bun("Булочка", 4646464);
-     assertEquals("Булочка", bun.getName());
+     Bun bun = new Bun(name, price);
+     assertEquals(expectedName, bun.getName());
  }
 
     @Test
     public void getPriceTest(){
-        Bun bun = new Bun("Булочка", 4646464);
-        assertEquals(4646464, bun.getPrice(), 0);
+        Bun bun = new Bun(name, price);
+        assertEquals(expectedPrice, bun.getPrice(), 0);
     }
 }
