c5fd6b76bdaf0a1c96b9acd0ccb0fff7f72484fb
Elena Samsonova <lenusyochek@rambler.ru>
2022-12-25 11:40:53 +0300

    third commit

--- src/test/java/BunParameterizedTest.java
+++ src/test/java/BunParameterizedTest.java
@@ -4,16 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(Parameterized.class)
-public class BunParameterizedNameTest {
+public class BunParameterizedTest {
     private final String name;
     private final float price;
     private final String expectedName;
     private final float expectedPrice;
 
-    public BunParameterizedNameTest(String name, float price, String expectedName, float expectedPrice) {
+    public BunParameterizedTest(String name, float price, String expectedName, float expectedPrice) {
         this.name=name;
         this.expectedName=expectedName;
         this.price=price;
@@ -23,10 +21,8 @@ public class BunParameterizedNameTest {
     public static Object[][] getNameData() {
         return new Object[][] {
                 {"black bun", 100, "black bun", 100},
-                {null, null, null, null},
                 {"", 99999, "", 99999},
                 {"sooooooolooooongnaaaaame", -100, "sooooooolooooongnaaaaame", -100},
-                {"//////", 100.5, "//////", 100.5},
                 {"         ", 0, "         ", 0}
         };
     }
