a289b4256b4bbc7b1387803fa67efd2fc43d50f9
DmitriyKnol <Dmi3ydik@yandex.ru>
2022-12-27 02:27:26 +0500

    Add README.MD Add IngredientTest

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,17 @@
+//import net.bytebuddy.matcher.ElementMatcher;
+//import org.junit.Test;
+//import praktikum.IngredientType;
+//
+//import static net.bytebuddy.matcher.ElementMatchers.is;
+//import static org.hamcrest.CoreMatchers.notNullValue;
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.junit.Assert.assertEquals;
+//
+//public class IngredientTypeTest {
+//   @Test
+//   public void ingredientTypeIsNotNull() {
+//    assertThat(IngredientType.FILLING, is(notNullValue()));
+//}
+//
+//
+//}
