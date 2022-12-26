4a4bc8df6c6201a44f4bd4c9760493c27ce1105b
DmitriyKnol <Dmi3ydik@yandex.ru>
2022-12-26 21:02:32 +0500

    Add test to class Bun

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import praktikum.Bun;
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+    private final String name = "Вкусняга";
+    private final float price = 237;
+
+
+    Bun bun = new Bun(name, price);
+    @Test
+    public void getNameReturnName() {
+
+        assertEquals("Вкусняга", bun.getName());
+    }
+
+    @Test
+    public void getPriceReturnPrice() {
+        assertEquals(237.0, bun.getPrice(), 0.01);
+    }
+}
