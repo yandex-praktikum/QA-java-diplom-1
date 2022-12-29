9ee2087d494d19f33739bf8c1c59897a0a9b4975
Tatyana Manaskina <bulochka_s_shokoladom@mail.ru>
2022-12-29 15:03:51 +0300

    Создан тестовый класс для Bun с моками и стабами.

---
+++ src/test/java/BunMockTest.java
@@ -0,0 +1,41 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BunMockTest {
+
+    @Mock
+    Bun bunMock;
+
+    @Test
+    public void getNameBunTest(){
+        bunMock.getName();
+        bunMock.getName();
+        Mockito.verify(bunMock, Mockito.times(2)).getName();
+    }
+
+    @Test
+    public void getNameBunStabTest(){
+        Mockito.when(bunMock.getName()).thenReturn("Круассан");
+        Assert.assertEquals("Круассан", bunMock.getName());
+    }
+
+    @Test
+    public void getPriceBunTest(){
+        bunMock.getPrice();
+        bunMock.getPrice();
+        bunMock.getPrice();
+        Mockito.verify(bunMock, Mockito.times(3)).getPrice();
+    }
+
+    @Test
+    public void getPriceBunStabTest(){
+        Mockito.when(bunMock.getPrice()).thenReturn(5000.0f);
+        Assert.assertEquals(5000.0f, bunMock.getPrice(), 0);
+    }
+}
