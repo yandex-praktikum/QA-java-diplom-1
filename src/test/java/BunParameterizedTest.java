d0e434c92e379a8fe8fb6406868c16197ffe4a79
Tatyana Manaskina <bulochka_s_shokoladom@mail.ru>
2022-12-29 15:04:51 +0300

    Создан тестовый класс для класса Bun с параметризацией.

---
+++ src/test/java/BunParameterizedTest.java
@@ -0,0 +1,41 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+
+@RunWith(Parameterized.class)
+public class BunParameterizedTest {
+
+        private String name;
+        private float price;
+
+        public BunParameterizedTest(String name, float price){
+                this.name = name;
+                this.price = price;
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] getBunParameters(){
+                return new Object[][]{
+                        {"Круассан", 50.0f},
+                        {"Булочка с чернилами каракатицы", 100.5f},
+                        {"Вселенская булка Таноса", 234.56f},
+                };
+        }
+
+        @Test
+        public void getNameBunParameterizedTest(){
+                Bun bun = new Bun(name,price);
+                Assert.assertEquals(name, bun.getName());
+        }
+
+        @Test
+        public void getPriceBunParameterizedTest(){
+                Bun bun = new Bun(name, price);
+                Assert.assertEquals(price, bun.getPrice(),0);
+        }
+
+
+
+}
