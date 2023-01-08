eee621e02a65376fa3f75177b94b10b1f5f870fb
Julia Evteeva <ylia.evteeva@mail.ru>
2023-01-08 19:13:55 +0300

    add all my tests

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertNotNull;
+
+public class IngredientTypeTest {
+    @Test
+    public void testSauceIsNotNull() {
+        assertNotNull(IngredientType.SAUCE);
+    }
+
+    @Test
+    public void testFillingIsNotNull() {
+        assertNotNull(IngredientType.FILLING);
+    }
+}
