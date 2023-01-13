fb6577f2f77ca04bcc954f9c95e801feb5b423e7
Andrey Anisimov <a.a._anisimov@mail.ru>
2023-01-14 02:16:07 +0300

    add params test (at the request of the task)

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -2,21 +2,51 @@ package praktikum;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
-
+@RunWith(Parameterized.class)
 public class IngredientTest {
-    Ingredient ingredient = new Ingredient(SAUCE, "майонезик",1000);
+    @Parameterized.Parameter(0)
+    public IngredientType type;
+
+    @Parameterized.Parameter(1)
+    public String name;
+
+    @Parameterized.Parameter(2)
+    public float price;
+
+    @Parameterized.Parameters(name = "Type: {0}, Name: {1}, Price: {2}")
+    public static Object[][] params() {
+        return new Object[][]{
+                {SAUCE, "майонезик", 500},
+                {FILLING, "кекчук", 600},
+        };
+    }
     @Test
     public void getPrice() {
-        Assert.assertEquals(1000, ingredient.getPrice(),0);
+        if (price == 500) {
+            Assert.assertEquals(500, price, 0);
+        } else if (price == 600) {
+            Assert.assertEquals(600, price, 0);
+        }
     }
     @Test
     public void getName() {
-        Assert.assertEquals("майонезик", ingredient.getName());
+        if (name == "майонезик") {
+            Assert.assertEquals("майонезик", name);
+        } else if (name == "кекчук") {
+            Assert.assertEquals("кекчук", name);
+        }
     }
     @Test
     public void getType() {
-        Assert.assertEquals(SAUCE, ingredient.getType());
+        if (type == SAUCE) {
+            Assert.assertEquals(SAUCE, type);
+        } else if (type == FILLING) {
+            Assert.assertEquals(FILLING, type);
+        }
     }
 }
