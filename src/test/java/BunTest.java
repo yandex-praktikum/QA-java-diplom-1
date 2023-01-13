81b565bd320db25435292c549a6e650d26174414
anton vinokurov <anton.vinokurov.qa@gmail.com>
2023-01-13 18:50:23 +0100

    bun tests

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,18 @@
+import data.Data;
+import org.junit.Assert;
+import org.junit.Test;
+import praktikum.Bun;
+
+public class BunTest {
+    @Test
+    public void checkThatBunGetsName(){
+        Bun bun = new Bun(Data.BLACK_BUN, Data.BLACK_BUN_PRICE);
+        Assert.assertEquals(Data.BLACK_BUN, bun.getName());
+    }
+
+    @Test
+    public void checkThatBunGetsPrice(){
+        Bun bun = new Bun(Data.BLACK_BUN, Data.BLACK_BUN_PRICE);
+        Assert.assertEquals(Data.BLACK_BUN_PRICE, bun.getPrice(), 0.0f);
+    }
+}
---
+++ src/test/java/data/Data.java
@@ -0,0 +1,9 @@
+package data;
+
+public class Data {
+    public static final String BLACK_BUN = "BLACK";
+    public static final float BLACK_BUN_PRICE = 10.1F;
+    public static final String STEAK = "STEAK";
+    public static final float STEAK_PRICE = 12.1F;
+
+}
