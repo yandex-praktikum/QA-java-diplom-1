c301170680de8ee7c64e92c3061804d173688a6e
Roman Ramazanov <mark.n.81@bk.ru>
2023-01-18 01:46:51 +0300

    fix!

--- src/test/java/bun/BunTest.java
+++ src/test/java/bun/BunTest.java
@@ -1,20 +1,46 @@
 package bun;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
-    private final String name = "Вкусняшка";
-    private final float price = 666;
-    Bun bun = new Bun(name, price);
+    private final String name;
+    private final float price;
+    private Bun bun;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"orange bun", 300},
+                {"black bun", 150},
+                {"red bun", 200},
+                {null, 300},
+                {"bun", 0},
+                {"", 123f},
+        };
+    }
+
+    @Before
+    public void init() {
+        bun = new Bun(name, price);
+    }
 
     @Test
     public void getNameReturnName() {
-        assertEquals("Вкусняшка", bun.getName());
+        assertEquals("Имя не валидное!", name, bun.getName());
     }
 
     @Test
     public void getPriceReturnPrice() {
-        assertEquals(666.0, bun.getPrice(), 0.01);
+        assertEquals("Цена не валидна!", price, bun.getPrice(), 0.0f);
     }
 }
