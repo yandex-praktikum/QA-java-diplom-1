419aa2f494c624ab8020bf2760937ddee7d30c97
Windostre <windostre@gmail.com>
2023-01-20 16:43:59 +0300

    Созданы тестовые классы для классов Bun, Burger, Ingredient

---
+++ src/test/java/praktikum/BurgerTest.java
@@ -0,0 +1,38 @@
+package praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+@RunWith(MockitoJUnitRunner.class)
+public class BurgerTest {
+    @Mock
+    Bun bun;
+
+
+    @Test
+    public void setBuns() {
+    }
+
+    @Test
+    public void addIngredient() {
+    }
+
+    @Test
+    public void removeIngredient() {
+    }
+
+    @Test
+    public void moveIngredient() {
+    }
+
+    @Test
+    public void getPrice() {
+    }
+
+    @Test
+    public void getReceipt() {
+    }
+}
---
+++ src/test/java/praktikum/IngredientTest.java
@@ -0,0 +1,20 @@
+package praktikum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IngredientTest {
+
+    @Test
+    public void getPrice() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getType() {
+    }
+}
