17ecf1b48c933fa91220f54f2d9079ba2cdd5d9e
Viktor Biryukov <viktorbiryuko@digitalagro.ru>
2023-01-21 19:19:57 +0300

    Первая попытка юнит тестов для дипломной работы

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class BunTest {
+    Bun bun;
+
+    String name;
+
+    float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: Булочка - {0} Цена - {1}")
+    public static Object[][] getData() {
+        return new Object[][] {
+                {"Пшеничная", 100},
+                {"Ржаная", 120}
+        };
+    }
+
+    @Before
+    public void preBun() {
+        bun = new Bun(name,price);
+    }
+
+    @Test
+    public void checkGetName() {
+        assertEquals(name, bun.getName());
+    }
+
+    @Test
+    public void checkGetPrice() {
+        assertEquals(price, bun.getPrice(), 0);
+    }
+}
