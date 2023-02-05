b8cfc2d3dd667ac6df4edb42d3a90576da935600
Polina Iasakova <p.iasakova@hh.ru>
2023-02-05 09:52:47 +0300

    1 commit

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,29 @@
+package praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class BunTest {
+
+    @Mock
+    Bun bun;
+
+    @Test
+    public void testGetName() {
+        Bun bun = new Bun("black bun", 1f);
+        assertEquals("black bun", bun.getName());
+    }
+
+    @Test
+    public void testGetPrice() {
+        Bun bun = new Bun("black bun", 1f);
+        assertEquals(1f, bun.getPrice(), 0);
+    }
+
+}
