93035bdb1957688237dd0c7d4cac135f083fbc9a
Andrewravd <82881494+Andrewravd@users.noreply.github.com>
2023-02-08 03:19:22 +0300

    добавил юнит тесты

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -3,7 +3,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
