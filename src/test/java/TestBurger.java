9ab5ee8a6e52516b49a51a66912af829004f486a
langrek0426 <langrek0426@gmail.com>
2023-02-16 17:24:37 +0300

    fix review

--- src/test/java/TestBurger.java
+++ src/test/java/TestBurger.java
@@ -34,4 +34,45 @@ public class TestBurger {
         String actualReceipt = burger.getReceipt();
         Assert.assertEquals(expectedString, actualReceipt);
     }
+
+    @Test
+    public void addIngredientTest() {
+        Bun bun = new Bun ("Bun", 120);
+        Ingredient sauce = new Ingredient(IngredientType.SAUCE, "Red", 50);
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(sauce);
+        String expectedString = "(==== Bun ====)\n= sauce Red =\n(==== Bun ====)\n\nPrice: 290,000000\n";
+        String actualReceipt = burger.getReceipt();
+        Assert.assertEquals(expectedString, actualReceipt);
+    }
+
+    @Test
+    public void removeIngredientTest() {
+        Bun bun = new Bun ("Bun", 120);
+        Ingredient sauce = new Ingredient(IngredientType.SAUCE, "Red", 50);
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(sauce);
+        burger.removeIngredient(0);
+        String expectedString = "(==== Bun ====)\n(==== Bun ====)\n\nPrice: 240,000000\n";
+        String actualReceipt = burger.getReceipt();
+        Assert.assertEquals(expectedString, actualReceipt);
+    }
+
+    @Test
+    public void moveIngredientTest() {
+        Bun bun = new Bun ("Bun", 120);
+        Ingredient sauce = new Ingredient(IngredientType.SAUCE, "Red", 50);
+        Ingredient filling = new Ingredient(IngredientType.FILLING, "Cheese", 10);
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(sauce);
+        burger.addIngredient(filling);
+        burger.moveIngredient(1, 0);
+        String expectedString = "(==== Bun ====)\n= filling Cheese =\n= sauce Red =\n(==== Bun ====)\n\nPrice: 300,000000\n";
+        String actualReceipt = burger.getReceipt();
+        Assert.assertEquals(expectedString, actualReceipt);
+    }
+
 }
