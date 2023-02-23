0159801d97f6650bd84417ac6df7c5debd2f8020
CherezBereg <117637101+CherezBereg@users.noreply.github.com>
2023-02-23 21:28:36 +0300

    v.1.0

---
+++ src/test/java/BunTests.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+
+@RunWith(Parameterized.class)
+public class BunTests {
+    private final String name;
+    private final float price;
+
+    public BunTests(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: {0} {1}")
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"English bun", 999},
+                {"Кунжутная", 1000},
+                {"", 10},
+                {"3412", 0},
+                {"%@#&6af_Ру", -1}
+        };
+    }
+
+    @Test
+    public void getNameTest() {
+        Bun bun = new Bun(name, price);
+        Assert.assertEquals(name, bun.getName());
+    }
+
+    @Test
+    public void getPriceTest() {
+        Bun bun = new Bun(name, price);
+        Assert.assertEquals(price, bun.getPrice(), 0);
+    }
+
+}
