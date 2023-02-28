d3a77172291884491ada76c44e4374ce4c046b96
Olga Bileva <a.niramas@yandex.ru>
2023-02-28 20:55:37 +0300

    bun test

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class BunTest {
+import org.junit.Before;
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    private Bun bun1;
+    private static final String BUN_NAME = "Пирожок";
+    private static final float BUN_PRICE = 18.44f;
+
+    @Before
+    public void setUp() {
+        bun1 = new Bun(BUN_NAME, BUN_PRICE);
+    }
+
+    @Test
+    public void getNameTest() {
+        assertEquals(BUN_NAME, bun1.getName());
+    }
+
+    @Test
+    public void getNamePrice() {
+        assertEquals(BUN_PRICE, bun1.getPrice(), 0.0001);
+    }
+
 }
