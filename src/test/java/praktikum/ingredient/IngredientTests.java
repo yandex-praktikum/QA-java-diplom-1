dbcc68aa4cb53bc9556f56a893481c8d88ea3f89
Ekaterina Lepekhina <katrin.le99@yandex.ru>
2023-03-19 09:19:46 +0300

    ingredient tests

---
+++ src/test/java/praktikum/ingredient/IngredientTests.java
@@ -0,0 +1,43 @@
+package praktikum.ingredient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
+
+@RunWith(Parameterized.class)
+public class IngredientTests {
+
+    private IngredientType type;
+    private String name;
+    private float price;
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {FILLING, "Салат", 2.0F},
+                {SAUCE, "Сальса", 3.0F},
+                {FILLING, "Котлета", 7.0F}
+        };
+    }
+
+    public IngredientTests(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Test
+    public void checkIngredientValues() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals("Invalid ingredient type", type, ingredient.getType());
+        assertEquals("Invalid name", name, ingredient.getName());
+        assertEquals("Invalid price", price, ingredient.getPrice(), 0.0F);
+    }
+
+}
