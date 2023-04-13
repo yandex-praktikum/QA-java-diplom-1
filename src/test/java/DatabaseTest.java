74a641c789417cbeace9c6132729000aae943767
Firuz Aminov <firuzaminoff@vk.com>
2023-04-13 20:06:57 +0300

    add jacoco report

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,26 +1,2 @@
-import org.junit.Test;
-import praktikum.Bun;
-import praktikum.Database;
-
-import java.util.ArrayList;
-import java.util.List;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-public class DatabaseTest {
-
-    @Test
-    public void testAvailableBuns() {
-        Database database = new Database();
-        List<Bun> buns = database.availableBuns();
-        assertNotNull(buns);
-    }
-
-    @Test
-    public void testAvailableBunsSize() {
-        Database database = new Database();
-        List<Bun> buns = database.availableBuns();
-        assertEquals(3, buns.size());
-    }
-
+package PACKAGE_NAME;public class DatabaseTest {
 }
