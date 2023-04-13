e6ad05f91faa34469dfdab185ee586041af2b0d3
Nauruz Agoev <nz.agoev@gmail.com>
2023-04-13 16:39:50 +0300

    fixed and add jacoco report

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -5,25 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
-
     private Burger burger;
-
     @Mock
     private Bun bun;
-
     @Mock
-    private Ingredient ingredient1;
-
+    private Ingredient firstIngredient;
     @Mock
-    private Ingredient ingredient2;
+    private Ingredient secondIngredient;
 
     @Before
     public void setUp() {
@@ -32,54 +26,53 @@ public class BurgerTest {
     }
     @Test
     public void testAddIngredient() {
-        burger.addIngredient(ingredient1);
+        burger.addIngredient(firstIngredient);
         assertEquals(1, burger.ingredients.size());
     }
     @Test
     public void testRemoveIngredient() {
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
         burger.removeIngredient(0);
         assertEquals(1, burger.ingredients.size());
     }
     @Test
     public void testMoveIngredient() {
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
         burger.moveIngredient(0, 1);
-        assertEquals(Arrays.asList(ingredient2, ingredient1), burger.ingredients);
+        assertEquals(Arrays.asList(secondIngredient, firstIngredient), burger.ingredients);
     }
     @Test
     public void testGetPrice() {
         when(bun.getPrice()).thenReturn(100f);
-        when(ingredient1.getPrice()).thenReturn(50f);
-        when(ingredient2.getPrice()).thenReturn(75f);
+        when(firstIngredient.getPrice()).thenReturn(50f);
+        when(secondIngredient.getPrice()).thenReturn(75f);
 
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
 
         assertEquals(325f, burger.getPrice(), 0);
     }
     @Test
     public void testGetReceipt() {
         when(bun.getName()).thenReturn("Black bun");
-        when(ingredient1.getType()).thenReturn(IngredientType.FILLING);
-        when(ingredient1.getName()).thenReturn("Cutlet");
-        when(ingredient1.getPrice()).thenReturn(100f);
-        when(ingredient2.getType()).thenReturn(IngredientType.SAUCE);
-        when(ingredient2.getName()).thenReturn("Hot sauce");
-        when(ingredient2.getPrice()).thenReturn(50f);
+        when(firstIngredient.getType()).thenReturn(IngredientType.FILLING);
+        when(firstIngredient.getName()).thenReturn("Cutlet");
+        when(firstIngredient.getPrice()).thenReturn(100F);
+        when(secondIngredient.getType()).thenReturn(IngredientType.SAUCE);
+        when(secondIngredient.getName()).thenReturn("Hot sauce");
+        when(secondIngredient.getPrice()).thenReturn(50F);
 
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
 
         String expected = "(==== Black bun ====)\n" +
                 "= filling Cutlet =\n" +
                 "= sauce Hot sauce =\n" +
                 "(==== Black bun ====)\n" +
                 "\n" +
-                "Price: 250.0\n";
-
+                "Price: 150,000000\n";
         assertEquals(expected, burger.getReceipt());
     }
 }
