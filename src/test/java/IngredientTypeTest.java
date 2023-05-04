daa38aadd34b6a8da71ea8d505314fb9bd18d225
cinanna <cinnabarworld@gmail.com>
2023-05-04 16:02:46 +0700

    add parametrization

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -2,13 +2,21 @@ import org.junit.Assert;
 import org.junit.Test;
 import praktikum.IngredientType;
 
+import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
 
 public class IngredientTypeTest {
-    public IngredientType type;
+    private IngredientType type;
+
     @Test
-    public void setType(){
+    public void setTypeSauce(){
         type = SAUCE;
         Assert.assertEquals(SAUCE, type);
+    }
+
+    @Test
+    public void setTypeFilling(){
+        type = FILLING;
+        Assert.assertEquals(FILLING, type);
     }
 }
