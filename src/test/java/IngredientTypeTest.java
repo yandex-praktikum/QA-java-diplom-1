a940e9ee442d14a173aa623a136c6c524282ccaf
Tamilla <68143196+ShomeRondo@users.noreply.github.com>
2023-05-08 12:21:43 +0300

    fix for IngredinetTypeTest

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -6,8 +6,12 @@ import static org.junit.Assert.assertEquals;
 public class IngredientTypeTest {
 
     @Test
-    public void checkIngredientType(){
+    public void checkIngredientTypeSauce(){
         assertEquals(IngredientType.valueOf("SAUCE"), IngredientType.SAUCE);
+    }
+
+    @Test
+    public void checkIngredientTypeFilling(){
         assertEquals(IngredientType.valueOf("FILLING"), IngredientType.FILLING);
     }
 }
