2f398c02bbf8aac5dccd6876eb0cde983bf6fe8d
Elena Vorobeva <yankina.elena.nso@yandex.ru>
2023-05-11 22:31:39 +0700

    Исправление замечаний

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -2,25 +2,48 @@ package praktikum;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class BunTest {
-  private Bun bun;
-    @Before
-    public void createNewBunCase() {
-         bun = new Bun("Флюоресцентная булка R2-D3", 1976.0f);
+
+    private final String name;
+    private final float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters(name = "Тестовое имя: {0}, Тестовая цена: {1}")
+    public static Object[][] getData() {
+        return new Object[][] {
+                {"bun", 10},
+                {"", 10},
+                {" ", 10},
+                {" bun", 10},
+                {"#bun", 10},
+                {"bunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbunbun", 10},
+                {"tasty bun", 10},
+                {"bun", 0},
+                {"bun", -1},
+                {"bun", Float.MIN_VALUE},
+                {"bun",3.4E+38f},
+        };
     }
 
     @Test
-    public void getNameTest()
-    {
-        assertEquals("Проверка названия булочки","Флюоресцентная булка R2-D3", bun.getName());
+    public void getNameTest() {
+        Bun bun = new Bun(name, price);
+        assertEquals("Имя", name, bun.getName());
     }
-    @Test
-    public void getPriceTest()
-    {
-        assertEquals("Проверка стоимости",1976.0f, bun.getPrice(), 0);
 
+    @Test
+    public void getPriceTest() {
+        Bun bun = new Bun(name, price);
+        assertEquals("Цена",price, bun.getPrice(), 0);
     }
 }
