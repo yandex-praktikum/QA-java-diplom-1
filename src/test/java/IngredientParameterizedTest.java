1cbaf0769d30f2d3d6c8b406133730dd22f7cf01
Denis Guchkov <denistomskk@yandex.ru>
2023-05-16 01:37:54 +0700

    Ingredient

---
+++ src/test/java/IngredientParameterizedTest.java
@@ -0,0 +1,54 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
+
+@RunWith(Parameterized.class)
+public class IngredientParameterizedTest {
+    private final IngredientType type;
+    private Ingredient ingredient;
+    private final String name;
+    private final float price;
+
+    public IngredientParameterizedTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+
+    }
+
+    @Parameterized.Parameters()
+    public static Object[][] getTestData() {
+        return new Object[][] {
+                {SAUCE, "hot sauce", 100},
+                {SAUCE, "sour cream", 200},
+                {SAUCE, "chili sauce", 300},
+                {SAUCE, "cutlet", 100},
+                {SAUCE, "dinosaur", 200},
+                {SAUCE, "sausage", 300},
+                {null, "cutlet", 100},
+                {FILLING, "", 2},
+                {FILLING, null, 15.08f}
+        };
+    }
+
+    @Before
+    public void startTest() { ingredient = new Ingredient(type, name, price); }
+
+    @Test
+    public void checkGetValidPrice() { assertEquals("invalid name", price, ingredient.getPrice()); }
+
+    @Test
+    public void checkGetValidName() { assertEquals("invalid name", name, ingredient.getName()); }
+
+    @Test
+    public void checkGetValidType() {
+        assertEquals("invalid type", type, ingredient.getType());
+    }
+}
