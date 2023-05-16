6711b238dfb566764e87582daaf9321e1247b394
Pavel Vinogradov <pav.vinogradow1@yandex.ru>
2023-05-16 12:14:42 +0300

    add Ingredient types tests

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+import praktikum.IngredientType;
+
+public class IngredientTypeTest {
+    @Test
+    public void testSauceAvailable(){
+        Assert.assertNotNull("Такого соуса не существует", IngredientType.valueOf("SAUCE"));
+    }
+    @Test
+    public void testFillingAvailable(){
+        Assert.assertNotNull("Такой начинки нет", IngredientType.valueOf("FILLING"));
+    }
+}
