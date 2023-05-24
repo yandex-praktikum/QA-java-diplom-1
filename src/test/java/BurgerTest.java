cc8422a305443d8ef1b62330ced9a76942f7a855
Анна Николаева <amnik21@yandex.ru>
2023-05-24 21:15:33 +0300

    Написала тест на метод getPrice

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
@@ -14,6 +15,10 @@ public class BurgerTest {
     private Bun bun;
     @Mock
     private Ingredient ingredient;
+
+    private final static int INDEX = 0;
+    private final static int NEW_INDEX = 1;
+    private final static float EXPECTED_PRICE = 300;
 
     @Test
     public void testSetBuns() {
@@ -33,17 +38,35 @@ public class BurgerTest {
 
     @Test
     public void testRemoveIngredient() {
-
+        Burger burger = new Burger();
+        Assert.assertEquals(burger.ingredients.size(), 0);
+        burger.addIngredient(ingredient);
+        Assert.assertEquals(burger.ingredients.size(), 1);
+        burger.removeIngredient(INDEX);
+        Assert.assertEquals(burger.ingredients.size(), 0);
     }
 
     @Test
     public void testMoveIngredient() {
-
+        Burger burger = new Burger();
+        Assert.assertEquals(burger.ingredients.size(), 0);
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient);
+        Assert.assertEquals(burger.ingredients.size(), 2);
+        burger.moveIngredient(INDEX, NEW_INDEX);
+        Assert.assertEquals(burger.ingredients.get(NEW_INDEX), burger.ingredients.get(INDEX));
     }
 
     @Test
     public void testGetPrice() {
-
+        Burger burger = new Burger();
+        Assert.assertEquals(burger.ingredients.size(), 0);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Mockito.when(bun.getPrice()).thenReturn(100F);
+        Mockito.when(ingredient.getPrice()).thenReturn(100F);
+        float actualPrice = burger.getPrice();
+        Assert.assertEquals(EXPECTED_PRICE, actualPrice, 0.05);
     }
 
     @Test
