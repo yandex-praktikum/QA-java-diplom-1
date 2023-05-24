a72e4549e64f5c3fe30b7673792eb9671af230ef
Анна Николаева <amnik21@yandex.ru>
2023-05-24 22:46:05 +0300

    Написала тест на метод getReceipt

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -7,6 +7,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
@@ -19,6 +20,12 @@ public class BurgerTest {
     private final static int INDEX = 0;
     private final static int NEW_INDEX = 1;
     private final static float EXPECTED_PRICE = 300;
+    private final static String EXPECTED_RECEIPT = "(==== white bun ====)\n" +
+                                                    "= sauce sour cream =\n" +
+                                                    "= sauce sour cream =\n" +
+                                                    "(==== white bun ====)\n" +
+                                                    "\n" +
+                                                    "Price: 400,000000\n";
 
     @Test
     public void testSetBuns() {
@@ -63,6 +70,7 @@ public class BurgerTest {
         Assert.assertEquals(burger.ingredients.size(), 0);
         burger.setBuns(bun);
         burger.addIngredient(ingredient);
+        Assert.assertEquals(burger.ingredients.size(), 1);
         Mockito.when(bun.getPrice()).thenReturn(100F);
         Mockito.when(ingredient.getPrice()).thenReturn(100F);
         float actualPrice = burger.getPrice();
@@ -71,6 +79,17 @@ public class BurgerTest {
 
     @Test
     public void testGetReceipt() {
-
+        Burger burger = new Burger();
+        Assert.assertEquals(burger.ingredients.size(), 0);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient);
+        Assert.assertEquals(burger.ingredients.size(), 2);
+        Mockito.when(bun.getName()).thenReturn("white bun");
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(ingredient.getName()).thenReturn("sour cream");
+        Mockito.when(ingredient.getPrice()).thenReturn(200F);
+        String actualReceipt = burger.getReceipt();
+        Assert.assertEquals(EXPECTED_RECEIPT, actualReceipt);
     }
 }
