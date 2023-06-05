e322d3bb3c0b40eef84997c462cd42142d3dd73c
Alexandra <aleksandra.sagaidak@yandex.ru>
2023-06-05 12:21:43 +0300

    commit_Diplom_Part_1

---
+++ src/test/java/BurgerParamTest.java
@@ -0,0 +1,59 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import praktikum.*;
+
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
+
+@RunWith(Parameterized.class)
+public class BurgerParamTest {
+    private final IngredientType type;
+    private final String ingredientName;
+    private final float priceIngredient;
+    Burger burger;
+    @Mock
+    private Bun bun;
+    @Mock
+    private Ingredient ingredient;
+
+
+    public BurgerParamTest(IngredientType type, String ingredientName, float priceIngredient) {
+        this.type = type;
+        this.ingredientName = ingredientName;
+        this.priceIngredient = priceIngredient;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные:{0},{1}")
+    public static Object[][] nameIngredient() {
+        return new Object[][]{
+                {SAUCE, "sour cream", 200f},
+                {FILLING, "dinosaur", 200f}
+        };
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void getReceiptParameterized() {
+        burger = new Burger();
+        burger.bun = bun;
+        Mockito.when(bun.getName()).thenReturn("red bun");
+        Mockito.when(ingredient.getName()).thenReturn(ingredientName);
+        Mockito.when(ingredient.getPrice()).thenReturn(priceIngredient);
+        Mockito.when(ingredient.getType()).thenReturn(type);
+        String result = burger.getReceipt();
+        Assert.assertEquals("(==== red bun ====)\r\n" +
+                "(==== red bun ====)\r\n" +
+                "\r\n" +
+                "Price: 0,000000\r\n", result);
+    }
+}
---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+public class IngredientTypeTest {
+    public Ingredient ingredient;
+    @Mock
+    public IngredientType ingredientType;
+
+    @Before
+    public void setUp() {
+        ingredient = new Ingredient(ingredientType, "bacon", 5.5f);
+    }
+
+    @Test
+    public void getNameIngredient() {
+        Assert.assertEquals("bacon", ingredient.getName());
+    }
+
+    @Test
+    public void getPriceIngredient() {
+        Assert.assertEquals(5.5f, ingredient.getPrice(), 0.1f);
+    }
+}
