9cff9dd82f8091f0a3359945324d3a98a29ac833
Andrey Vorobev <mamin.xaker@yandex.ru>
2023-06-13 14:21:42 +0300

    Add test for class Bun

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Задание 1: юнит-тесты: #
 ### *Нужно протестировать программу, которая помогает заказать бургер в [Stellar Burgers](https://stellarburgers.nomoreparties.site/)*
-### В проекте используется *Java 11* ###
-### Библиотеки: ###
+**Что нужно сделать:**
+* Покрыть тестами классы **Bun, Burger, Ingredient, IngredientType**.
+Процент покрытия должен быть не ниже 70%.
+### В проекте используется: ###
+
+![Linkedin-url](https://img.shields.io/badge/Java-_11-red)
+#### Библиотеки: ####
+![Linkedin-url](https://img.shields.io/badge/Maven-version_4.0.0-blue)
+
 ![Linkedin-url](https://img.shields.io/badge/Jacoco-version_0.8.7-blue)
 
 ![Linkedin-url](https://img.shields.io/badge/Mockito-version_3.12.4-blue)
@@ -9,4 +16,6 @@
 ![Linkedin-url](https://img.shields.io/badge/JUnit_4-version_4.13.2-blue)
 
 ---
-###### Над проектом работал Andrey Vorobev
+
+---
+###### Над проектом работал [Andrey Vorobev](https://github.com/AndreyJVM)
--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,7 +1,48 @@
 package praktikum;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class BunTest {
 
+    private final String nameBun;
+    private final float priceBun;
+    private Bun bun;
+
+    @Before
+    public void setUp() {
+        bun = new Bun(nameBun, priceBun);
+    }
+
+    public BunTest(String nameBun, float priceBun) {
+        this.nameBun = nameBun;
+        this.priceBun = priceBun;
+    }
+
+    @Parameterized.Parameters(name = "nameBun: {0}; priceBun: {1}")
+    public static Object[][] getBun() {
+        return new Object[][]{
+                {"Меркурианская", 50.0f},
+                {"Space", 0},
+                {null, 50},
+                {"777", 0},
+                {"@#$%", -100.5f}
+        };
+    }
+
+    @Test
+    public void getNameExpectedEqualsActual() {
+        // {0 - expected} {1 - actual}
+        Assert.assertEquals(nameBun, bun.getName());
+    }
+
+    @Test
+    public void getPriceExpectedEqualsActual() {
+        // {1 - expected} {2 - actual}
+        Assert.assertEquals("Error getPrice", priceBun, bun.getPrice(), 0);
+    }
 }
