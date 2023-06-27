14e44d26e169efe4484e067788622a0ce4241333
Karen Karslyan <k.karslian@mail.ru>
2023-06-27 18:08:59 +0300

    BunTests ready

---
+++ src/test/java/bunTests/getBunDataReturnsDataTest.java
@@ -0,0 +1,29 @@
+package bunTests;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+
+@RunWith(MockitoJUnitRunner.class)
+public class getBunDataReturnsDataTest {
+    @Mock
+    Bun bun;
+    @Test
+    public void getBunNameReturnsNameTest() {
+        bun.getName();
+        Mockito.when(bun.getName()).thenReturn("булочка");
+        Assert.assertEquals("булочка", bun.getName());
+    }
+
+    @Test
+    public void getBunPriceReturnsPrice(){
+        bun.getPrice();
+        Mockito.when(bun.getPrice()).thenReturn(243F);
+        Assert.assertEquals(243F, bun.getPrice(), 0.5);
+    }
+}
