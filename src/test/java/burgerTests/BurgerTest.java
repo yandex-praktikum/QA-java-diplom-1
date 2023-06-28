ca26fc53acae06c19acde54fcc2b8daf0a11eed2
Karen Karslyan <k.karslian@mail.ru>
2023-06-28 22:20:04 +0300

    BurgerTests ready

--- src/test/java/burgerTests/BurgerTest.java
+++ src/test/java/burgerTests/BurgerTest.java
@@ -1,5 +1,6 @@
 package burgerTests;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,14 +8,41 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import praktikum.Bun;
 import praktikum.Burger;
+import praktikum.Database;
 import praktikum.Ingredient;
-import praktikum.IngredientType;
+
+import static praktikum.Burger.ingredients;
+
 
 public class BurgerTest {
+    private Burger burger;
+
+
+    private  Bun bun;
+
+    private Ingredient ingredient1;
+    private Ingredient ingredient2;
+
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
+        Database database = new Database();
+        burger = new Burger();
+        bun = database.availableBuns().get(1);
+     ingredient1 = database.availableIngredients().get(3);
+     ingredient2 = database.availableIngredients().get(1);
+        burger.addIngredient(ingredient1);
+        burger.addIngredient(ingredient2);
+        burger.setBuns(bun);
+
     }
+    @After
+    public void clearIngredientsAfter(){
+        for (int i = burger.ingredients.size(); i >= 1; i--) {
+            burger.removeIngredient(i - 1);
+        }
+    }
+
     @Test
 
     public void setBunsTest() {
@@ -22,52 +50,45 @@ public class BurgerTest {
         Burger burger = Mockito.mock(Burger.class);
         burger.setBuns(bun);
         Mockito.verify(burger).setBuns(Mockito.any());
-
-
-
     }
 
     @Test
     public void addIngredientTest() {
-        Ingredient ingredient = new Ingredient(IngredientType.FILLING, "cutlet", 100);
-        Burger burger = Mockito.mock(Burger.class);
-       burger.addIngredient(ingredient);
-       Mockito.verify(burger).addIngredient(Mockito.any());
-
+       Assert.assertNotNull( ingredients.get(0));
     }
 
     @Test
     public void removeIngredientTest() {
-
-        Ingredient ingredient1 = new Ingredient(IngredientType.FILLING, "cutlet", 100);
-        Burger burger = new Burger();
-        burger.addIngredient(ingredient1);
         burger.removeIngredient(0);
-        int ingredientSize = Burger.ingredients.size();
-        Assert.assertEquals (0, ingredientSize );
+        int ingredientSize = ingredients.size();
+        Assert.assertEquals (1, ingredientSize );
 
     }
 
     @Test
     public void moveIngredientTest() {
+        burger.moveIngredient(1,0);
+        Assert.assertEquals(ingredient2, ingredients.get(0));
 
     }
 
     @Test
     public void getPriceTest() {
-        Bun bun = new Bun("Булка", 200);
-        Ingredient ingredient = new Ingredient (IngredientType.FILLING, "Котлета", 200);
-        Burger burger = new Burger();
-        burger.setBuns(bun);
-        burger.addIngredient(ingredient);
-        Assert.assertEquals(600, burger.getPrice(), 0.5);
-
+        Assert.assertEquals(700, burger.getPrice(), 0.5);
     }
+
 
 
     @Test
     public void getReceiptTest() {
-        Bun bun = new Bun("Булка", 200);
+        String actualReceipt = burger.getReceipt();
+        String bunString = String.format("(==== %s ====)%n", bun.getName());
+        String sauceString = String.format("= %s %s =%n", ingredient1.getType().toString().toLowerCase(), ingredient1.getName());
+        String fillingString = String.format("= %s %s =%n", ingredient2.getType().toString().toLowerCase(), ingredient2.getName());
+        String priceString = String.format("%nPrice: %f%n", burger.getPrice());
+        Assert.assertEquals(bunString + sauceString + fillingString + bunString + priceString, actualReceipt);
+        System.out.println(bunString + sauceString + fillingString + bunString + priceString);
 
     }
+
 }
---
+++ src/test/java/ingredientTests/IngredientTest.java
@@ -0,0 +1,30 @@
+package ingredientTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IngredientTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getPrice() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getType() {
+    }
+}
