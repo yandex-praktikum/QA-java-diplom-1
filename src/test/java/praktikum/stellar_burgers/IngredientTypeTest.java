b759b97de24f032a30983e69ddadd47a9d8b686d
Sergei Kislenko <funclock.kislenko@yandex.ru>
2023-07-06 22:22:54 +0300

    - изменил тесты для IngredientTypeTest

--- src/test/java/praktikum/stellar_burgers/IngredientTypeTest.java
+++ src/test/java/praktikum/stellar_burgers/IngredientTypeTest.java
@@ -1,29 +1,17 @@
 package praktikum.stellar_burgers;
 
+import org.junit.Assert;
 import org.junit.Test;
 import praktikum.IngredientType;
-import static org.junit.Assert.assertEquals;
-import static praktikum.IngredientType.FILLING;
-import static praktikum.IngredientType.SAUCE;
 
 public class IngredientTypeTest {
-
-    IngredientType firstType = SAUCE;
-    IngredientType secondType = FILLING;
-
     @Test
-    public void ingredientValues() {
-        IngredientType[] allType = IngredientType.values();
-        assertEquals(firstType, allType[0]);
-        assertEquals(secondType, allType[1]);
-
+    public void ingredientTypeContainsSauceValue() {
+        Assert.assertNotNull(IngredientType.valueOf("SAUCE"));
     }
 
     @Test
-    public void ingredientValueOf() {
-        IngredientType firstActual = IngredientType.valueOf(firstType.toString());
-        IngredientType secondActual = IngredientType.valueOf(secondType.toString());
-        assertEquals(firstType, firstActual);
-        assertEquals(secondType, secondActual);
+    public void ingredientTypeContainsFillingValue() {
+        Assert.assertNotNull(IngredientType.valueOf("FILLING"));
     }
 }
