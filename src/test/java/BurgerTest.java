d0f29a5760601901a0ab7f67b0aaa719c88096d0
Ilya Peshkov <ilapeskov97@gmail.com>
2023-07-30 21:32:08 +0300

    Добавил моки в BurgerTest.java

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -9,79 +9,80 @@ import praktikum.Ingredient;
 import praktikum.IngredientType;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
-    Bun bunNew = new Bun("Обыкновенная булочка", 10.00f);
-    Ingredient ingredient1 = new Ingredient(IngredientType.SAUCE, "Огуречный", 5.00f);
-    Ingredient ingredient2 = new Ingredient(IngredientType.SAUCE, "Помидор", 10.00f);
-    Ingredient ingredient3 = new Ingredient(IngredientType.SAUCE, "Курица", 15.00f);
-    double expectedPrice = bunNew.getPrice() * 2 + ingredient1.getPrice() + ingredient2.getPrice() + ingredient3.getPrice();
-    String expectedReceipt =
+    private final static int INDEX = 0;
+    private final static int NEW_INDEX = 1;
+    private final static float EXPECTED_PRICE = 300;
+    private final static String EXPECTED_RECEIPT = "(==== Обыкновенная булочка ====)\r\n" +
+            "= sauce Огуречный =\r\n" +
+            "= sauce Огуречный =\r\n" +
             "(==== Обыкновенная булочка ====)\r\n" +
-                    "= sauce Огуречный =\r\n" +
-                    "= sauce Помидор =\r\n" +
-                    "= sauce Курица =\r\n" +
-                    "(==== Обыкновенная булочка ====)\r\n" +
-                    "\r\n" +
-                    "Price: 50,000000" +
-                    "\r\n";
+            "\r\n" +
+            "Price: 400,000000\r\n";
 
     @Mock
-    Bun bun;
+    private Bun bun;
+
+    @Mock
+    private Ingredient ingredient;
+
 
     @Test
     public void setNewBunTest() {
         Burger burger = new Burger();
-        burger.setBuns(bunNew);
-        Bun expected = new Bun("Обыкновенная булочка", 15.00f);
-        assertEquals(expected.getName(), burger.bun.getName());
+        burger.setBuns(bun);
+        assertEquals(burger.bun, bun);
     }
 
     @Test
     public void addIngredientInBurgerTest() {
         Burger burger = new Burger();
-        burger.addIngredient(ingredient1);
-        assertEquals(burger.ingredients.get(0).getName(), "Огуречный");
+        burger.addIngredient(ingredient);
+        assertEquals(burger.ingredients.size(), 1);
     }
 
     @Test
     public void removeIngredientInBurgerTest() {
         Burger burger = new Burger();
-        burger.addIngredient(ingredient1);
-        burger.removeIngredient(0);
-        assertEquals(burger.ingredients.isEmpty(), true);
+        burger.addIngredient(ingredient);
+        burger.removeIngredient(INDEX);
+        assertTrue(burger.ingredients.isEmpty());
     }
 
     @Test
     public void moveIngredientsInList() {
         Burger burger = new Burger();
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-        burger.addIngredient(ingredient3);
-        burger.moveIngredient(0, 2);
-        assertEquals(burger.ingredients.get(0).getName(),"Помидор");
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient);
+        burger.moveIngredient(INDEX, NEW_INDEX);
+        assertEquals(burger.ingredients.get(NEW_INDEX), burger.ingredients.get(INDEX));
     }
 
     @Test
     public void getBurgerTotalPriceTest() {
         Burger burger = new Burger();
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-        burger.addIngredient(ingredient3);
-        Mockito.when(bun.getPrice()).thenReturn(50.00f);
-        Mockito.verify(bun, Mockito.times(1)).getPrice();
-        assertEquals(expectedPrice, burger.getPrice(), 0.00);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Mockito.when(bun.getPrice()).thenReturn(100F);
+        Mockito.when(ingredient.getPrice()).thenReturn(100F);
+        float actualPrice = burger.getPrice();
+        assertEquals(EXPECTED_PRICE, actualPrice, 0.05);
     }
 
     @Test
     public void getBurgerReceiptTest() {
         Burger burger = new Burger();
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-        burger.addIngredient(ingredient3);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient);
         Mockito.when(bun.getName()).thenReturn("Обыкновенная булочка");
-        Mockito.verify(bun, Mockito.times(2)).getName();
-        assertEquals(expectedReceipt, burger.getReceipt());
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(ingredient.getName()).thenReturn("Огуречный");
+        Mockito.when(ingredient.getPrice()).thenReturn(200F);
+        String actualReceipt = burger.getReceipt();
+        assertEquals(EXPECTED_RECEIPT, actualReceipt);
     }
 }
