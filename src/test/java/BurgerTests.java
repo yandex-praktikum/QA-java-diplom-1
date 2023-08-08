03e91c0bdc106d6392b3e3690bf711214cc5d1f7
Ekaterina Fedina <efedina05@yandex.ru>
2023-08-08 19:53:16 +0300

    Add 3 new Tests for Burger class

--- src/test/java/BurgerTests.java
+++ src/test/java/BurgerTests.java
@@ -1,2 +1,43 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
+
+    @Mock
+    Bun bun;
+    @Mock
+    Ingredient ingredient;
+
+    @Test
+    public void addIngredientTest() {
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+        Assert.assertEquals("Список ингридиентов должын содержать 1 элемент",
+                1, burger.ingredients.size());
+    }
+
+    @Test
+    public void removeIngredientTest() {
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+        burger.removeIngredient(0);
+        Assert.assertEquals("Список ингридиентов должен быть пустой",
+                0, burger.ingredients.size());
+    }
+
+    @Test
+    public void setBunsTest(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        Assert.assertEquals(bun,burger.bun);
+    }
+
+
 }
