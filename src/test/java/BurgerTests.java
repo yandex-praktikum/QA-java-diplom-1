44b2815998b00ceff5eb4fbc966b561c60d67bff
Ekaterina Fedina <efedina05@yandex.ru>
2023-08-09 11:55:24 +0300

    Final version

--- src/test/java/BurgerTests.java
+++ src/test/java/BurgerTests.java
@@ -11,6 +11,8 @@ import praktikum.Ingredient;
 import java.util.ArrayList;
 import java.util.List;
 
+import static praktikum.IngredientType.FILLING;
+
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
 
@@ -20,6 +22,7 @@ public class BurgerTests {
     Ingredient ingredient;
     @Mock
     Ingredient secondIngredient;
+
 
     @Test
     public void addIngredientTest() {
@@ -58,15 +61,31 @@ public class BurgerTests {
     }
 
     @Test
-    public void getPriceTest(){
+    public void getPriceTest() {
         Burger burger = new Burger();
         burger.setBuns(bun);
-        burger.addIngredient(ingredient);
+        burger.ingredients.add(ingredient);
         Mockito.when(bun.getPrice()).thenReturn(50.0F);
         Mockito.when(ingredient.getPrice()).thenReturn(20.0F);
         Assert.assertEquals("Бургер должен стоить 120 рублей",
-                120.0,burger.getPrice(),0.0);
+                120.0, burger.getPrice(), 0.0);
     }
 
-
+    @Test
+    public void getReceiptTest() {
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.ingredients.add(ingredient);
+        Mockito.when(bun.getName()).thenReturn("Ржаная");
+        Mockito.when(bun.getPrice()).thenReturn(60.0F);
+        Mockito.when(ingredient.getType()).thenReturn(FILLING);
+        Mockito.when(ingredient.getName()).thenReturn("Бекон");
+        Mockito.when(ingredient.getPrice()).thenReturn(100.0F);
+        String expected = "(==== Ржаная ====)\r\n" +
+                "= filling Бекон =\r\n" +
+                "(==== Ржаная ====)\r\n" +
+                "\r\n" +
+                "Price: 220,000000\r\n";
+        Assert.assertEquals(expected, burger.getReceipt());
+    }
 }
