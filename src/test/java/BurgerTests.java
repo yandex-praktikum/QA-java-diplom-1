05226ba0617182f21da16afeac64216d180811d0
Ekaterina Fedina <efedina05@yandex.ru>
2023-08-09 11:38:29 +0300

    Add more Tests for Burger class

--- src/test/java/BurgerTests.java
+++ src/test/java/BurgerTests.java
@@ -2,10 +2,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
@@ -14,6 +18,8 @@ public class BurgerTests {
     Bun bun;
     @Mock
     Ingredient ingredient;
+    @Mock
+    Ingredient secondIngredient;
 
     @Test
     public void addIngredientTest() {
@@ -33,10 +39,33 @@ public class BurgerTests {
     }
 
     @Test
-    public void setBunsTest(){
+    public void moveIngredientTest() {
+        Burger burger = new Burger();
+        burger.ingredients.add(ingredient);
+        burger.ingredients.add(secondIngredient);
+        List<Ingredient> expected = new ArrayList<>();
+        expected.add(secondIngredient);
+        expected.add(ingredient);
+        burger.moveIngredient(0, 1);
+        Assert.assertEquals(expected, burger.ingredients);
+    }
+
+    @Test
+    public void setBunsTest() {
         Burger burger = new Burger();
         burger.setBuns(bun);
-        Assert.assertEquals(bun,burger.bun);
+        Assert.assertEquals(bun, burger.bun);
+    }
+
+    @Test
+    public void getPriceTest(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Mockito.when(bun.getPrice()).thenReturn(50.0F);
+        Mockito.when(ingredient.getPrice()).thenReturn(20.0F);
+        Assert.assertEquals("Бургер должен стоить 120 рублей",
+                120.0,burger.getPrice(),0.0);
     }
 
 
