1691dd3f2895902a4a732fe328dd5d4d3c5a236e
Sergey Denisov <ytrewq7070@yandex.ru>
2023-08-11 17:32:24 +0300

    Создал тесты для основных классов по заданию.

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -6,8 +6,8 @@ import praktikum.Bun;
 
 @RunWith(Parameterized.class)
 public class BunTest {
-    public String name;
-    public float price;
+    String name;
+    float price;
     public BunTest(String name, float price) {
         this.name = name;
         this.price = price;
@@ -17,18 +17,17 @@ public class BunTest {
         return new Object[][] {
                 { "Достойная",9.99F},
                 { "По вкусу вкусная",7.5F },
-                { "По сути вкусная",5.0F },
+                { "По сути вкусная",5F },
         };
     }
-
     @Test
     public void getBunName() {
         Bun bun = new Bun(name, price);
-        Assert.assertEquals(bun.name, bun.getName());
+        Assert.assertEquals(name, bun.getName());
     }
     @Test
     public void getBunPrice() {
         Bun bun = new Bun(name, price);
-        Assert.assertEquals(bun.price, bun.getPrice(), 0);
+        Assert.assertEquals(price, bun.getPrice(), 0);
     }
 }
