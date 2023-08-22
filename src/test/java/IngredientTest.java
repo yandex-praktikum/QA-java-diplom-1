afc2e1e15c3a1a6f616f22b1d63251ab5786224d
Ella Yurochko <bcerga.ycnex@yandex.com>
2023-08-22 23:10:20 +0300

    added test

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -9,9 +8,9 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class IngredientTest {
-    private IngredientType type;
-    private String name;
-    private float price;
+    private final IngredientType type;
+    private final String name;
+    private final float price;
 
     public IngredientTest(IngredientType type, String name, float price){
         this.type = type;
