10275d354038c7e36eb376df9766007650eacb14
Radchenko Aleksey <radchenko.aleksei@gmail.com>
2023-08-27 20:31:35 +0300

    ver.1 All test ready. need to create report ver.2 Everything is done

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -1,3 +1,5 @@
+package praktikum;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,3 +1,5 @@
+package praktikum;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import praktikum.IngredientType;
