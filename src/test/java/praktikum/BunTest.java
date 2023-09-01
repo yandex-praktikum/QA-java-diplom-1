17ff4c562a6c33e8a8b43a442766abaf16468273
Radchenko Aleksey <radchenko.aleksei@gmail.com>
2023-09-01 18:25:16 +0300

    ver.3 Исправил pom, обновил тесты

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,42 +1,20 @@
 package praktikum;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-import java.util.Collection;
 
-@RunWith(Parameterized.class)
 public class BunTest {
-
-    private String name;
-    private float price;
-
-    public BunTest(String name, float price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                {"Bun1", 1.0f},
-                {"Bun2", 2.0f},
-                {"Bun3", 3.0f}
-        });
-    }
 
     @Test
     public void testName() {
-        Bun bun = new Bun(name, price);
-        assertEquals(name, bun.getName());
+        Bun bun = new Bun("Bun1", 1.0f);
+        assertEquals("Bun1", bun.getName());
     }
 
     @Test
     public void testPrice() {
-        Bun bun = new Bun(name, price);
-        assertEquals(price, bun.getPrice(), 0.01);
+        Bun bun = new Bun("Bun2", 2.0f);
+        assertEquals(2.0f, bun.getPrice(), 0.01);
     }
 }
