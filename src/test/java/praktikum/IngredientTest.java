ab949a895669b8941fe47780294f6365f7929b42
irina-maleeva <iwmaleeva@gmail.com>
2023-09-09 15:46:44 +0300

    with parametrization

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,32 +1,62 @@
 package praktikum;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
-    IngredientType type = IngredientType.SAUCE;
-    String ingredientName = "secret ingredient";
-    float ingredientPrice = 199;
+//    IngredientType type = IngredientType.SAUCE;
+//    String ingredientName = "secret ingredient";
+//    float ingredientPrice = 199;
+//
+//    Ingredient ingredient = new Ingredient(type, ingredientName, ingredientPrice);
+    private final IngredientType expectedType;
+    private final String expectedName;
+    private final float expectedPrice;
 
-    Ingredient ingredient = new Ingredient(type, ingredientName, ingredientPrice);
+    public IngredientTest(IngredientType expectedType, String expectedName, float expectedPrice) {
+        this.expectedType = expectedType;
+        this.expectedName = expectedName;
+        this.expectedPrice = expectedPrice;
+    }
+    Ingredient ingredient;
 
+        @Before
+    public void createIngredient() {
+            ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
+        }
+
+@Parameterized.Parameters
+public static Object[][] getIngredient(){
+    return new Object[][] {
+            {IngredientType.SAUCE, "hot sauce", 100},
+            {IngredientType.SAUCE, "sour cream", 200},
+            {IngredientType.SAUCE, "chili sauce", 300},
+            {IngredientType.FILLING, "cutlet", 100},
+            {IngredientType.FILLING, "dinosaur", 200},
+            {IngredientType.FILLING, "sausage", 300},
+    };
+}
 
     @Test
     public void testIngredientGetPrice() {
         float actualPrice = ingredient.getPrice();
-        assertEquals("wrong ingredient's price", ingredientPrice, actualPrice, 0);
+        assertEquals("wrong ingredient's price", expectedPrice, actualPrice, 0);
     }
 
     @Test
     public void testIngredientGetName() {
         String actualName = ingredient.getName();
-        assertEquals("wrong ingredient's name",ingredientName, actualName);
+        assertEquals("wrong ingredient's name",expectedName, actualName);
     }
 
     @Test
     public void testIngredientGetType() {
         IngredientType actualType = ingredient.getType();
-        assertEquals("wrong ingredient's type", type, actualType);
+        assertEquals("wrong ingredient's type", expectedType, actualType);
     }
 }
