339df8a259a726712d5dea50f8a71721d8075339
Magistrat <nisraton@gmail.com>
2023-10-07 23:07:48 +0300

    [feature] add test for Ingredients

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,7 +5,8 @@ public abstract class BaseTest {
     static final Random random = new Random();
     static final String bunName = "Флюоресцентная булка R2-D3";
     static final float bunPrice = random.nextFloat();
-    static final String sauceName = "Соус Spicy-X";
+    static final String ingredientSauceName = "Соус Spicy-X";
+    static final String ingredientFillingName = "Сыр с астероидной плесенью";
     static final float testDeltaRange = 0.0f;
 
 }
