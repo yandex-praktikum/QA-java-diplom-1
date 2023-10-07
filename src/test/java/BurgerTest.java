625a18e1a5b82d0ed6a5397d7060b53f4cc18b61
Magistrat <nisraton@gmail.com>
2023-10-07 23:43:57 +0300

    [feature] add test for Burger (without getReceipt method)

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,10 +1,19 @@
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
+import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
+import java.util.List;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTest extends BaseTest {
 
     @Mock
@@ -16,8 +25,12 @@ public class BurgerTest extends BaseTest {
     @Mock
     private static Ingredient secondIngredient;
 
+    private Burger burger;
+
     @Before
     public void setUpMock(){
+        burger = new Burger();
+
         Mockito.when(bun.getName()).thenReturn(bunName);
         Mockito.when(bun.getPrice()).thenReturn(random.nextFloat());
 
@@ -29,4 +42,41 @@ public class BurgerTest extends BaseTest {
         Mockito.when(secondIngredient.getName()).thenReturn(ingredientFillingName);
         Mockito.when(secondIngredient.getPrice()).thenReturn(random.nextFloat());
     }
+
+    @Test
+    public void setBunTest(){
+        burger.setBuns(bun);
+        Assert.assertEquals(bun, burger.bun);
+    }
+
+    @Test
+    public void addIngredientTest() {
+        burger.addIngredient(firstIngredient);
+
+        Assert.assertEquals(List.of(firstIngredient), burger.ingredients);
+        Assert.assertEquals(1, burger.ingredients.size());
+    }
+
+    @Test
+    public void moveIngredientToEndTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+
+        burger.moveIngredient(0, 1);
+
+        Assert.assertEquals(List.of(secondIngredient, firstIngredient), burger.ingredients);
+    }
+
+
+    @Test
+    public void getPriceTest() {
+        burger.setBuns(bun);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+
+        float expectedSum = bun.getPrice() * 2 + firstIngredient.getPrice() + secondIngredient.getPrice();
+
+        Assert.assertEquals(expectedSum, burger.getPrice(), testDeltaRange);
+    }
+
 }
