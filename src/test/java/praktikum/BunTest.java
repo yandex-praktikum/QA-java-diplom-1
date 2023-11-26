d4064075ce7b0cd41e30fbd488c3acc8ca67d46c
makarenko <t.makarenko@cft.ru>
2023-11-26 16:30:15 +0700

    Исправления после CR

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,28 +1,45 @@
 package praktikum;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    static Bun bun;
+    Bun bun;
+    String name;
+    float price;
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
 
-    @BeforeClass
-    public static void createBun() {
-        bun = new Bun("Moskovskaya", 25);
+    @Parameterized.Parameters(name = "Модель булочки для бургера. Тестовые данные: {0} {1}")
+    public static Object[][] getTestValues() {
+        return new Object[][]{
+                {"Moskovskaya", 25},
+                {"Saharnaya", 10.01f}
+        };
+    }
+    @Before
+    public void createBun() {
+        bun = new Bun(name, price);
     }
 
     @Test
     public void getName() {
-        String expectedName = "Moskovskaya";
+        String expectedName = name;
         assertEquals(expectedName, bun.getName());
     }
 
     @Test
     public void getPrice() {
-        float expectedPrice = 25;
+        float expectedPrice = price;
         assertEquals(expectedPrice, bun.getPrice(), 0.0);
     }
 }
--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
