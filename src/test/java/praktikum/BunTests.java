c300fa42587308007af8095ce45a390cad5aba8c
iozimniy <Kitriss@gmail.com>
2023-12-03 00:44:08 +0300

    add BunTests

---
+++ src/test/java/praktikum/BunTests.java
@@ -0,0 +1,40 @@
+package praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class BunTests {
+
+    private final String name;
+
+    private final float price;
+
+    public BunTests(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] setBun() {
+        return new Object[][]{
+                {"Флюоресцентная булка R2-D3", 988},
+                {"Краторная булка N-200i", 1255},
+                {"Булка цвета из иных миров", 2535.1F}
+        };
+    }
+
+    @Test
+    public void testBunGetName() {
+        Bun bun = new Bun(name, price);
+        assertEquals("Булка не отдаёт имя!", name, bun.getName());
+    }
+
+    @Test
+    public void testBunGetPrice() {
+        Bun bun = new Bun(name, price);
+        assertEquals("Булка бесценна!", price, bun.getPrice(), 0.05);
+    }
+}
