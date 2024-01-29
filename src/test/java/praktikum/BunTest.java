f2d680a6f9783d6d1a75e9fa4e70234d5524c761
kristinarudik <kristina.rudik@mvideo.ru>
2024-01-30 00:25:54 +0300

    added test for Bun

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,4 +1,33 @@
 package praktikum;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BunTest {
+
+
+	String bunName = RandomStringUtils.randomAlphabetic(7);
+	float bunPrice = Float.valueOf(RandomStringUtils.randomNumeric(3));
+
+	@Test
+	public void bunConstructorTest() {
+		Bun bun = new Bun(bunName, bunPrice);
+		assertEquals(bunName, bun.getName());
+		assertEquals(bunPrice, bun.getPrice(), 0);
+	}
+
+	@Test
+	public void getBunNameTest() {
+		Bun bun = new Bun(bunName, bunPrice);
+		Assert.assertEquals(bunName, bun.getName());
+	}
+
+	@Test
+	public void getBunPriceTest() {
+		Bun bun = new Bun(bunName, bunPrice);
+		Assert.assertEquals(bunPrice, bun.getPrice(), 0);
+	}
 }
