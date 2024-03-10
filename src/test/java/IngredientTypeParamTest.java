b32bfedc2bb58fc3d4d1c703e49351d19818ca4e
Julia Bouza <julia.jmpr@yandex.ru>
2024-03-10 22:02:40 +0300

    added tests and dependencies

---
+++ src/test/java/IngredientTypeParamTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IngredientTypeParamTest {
+    private final IngredientType type;
+    private final String name;
+
+    public IngredientTypeParamTest(IngredientType type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    @Parameterized.Parameters(name = "{index} : name = {0}")
+    public static Object[][] getIngredientTypeData() {
+        return new Object[][] {
+                {IngredientType.SAUCE, "SAUCE"},
+                {IngredientType.FILLING, "FILLING"}
+        };
+    }
+
+    @Test
+    public void checkIngredientTypeName() {
+        assertEquals("Incorrect name ingredient type", name, type.name());
+    }
+}
