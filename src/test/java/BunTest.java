d7b7c487784a47c5f2027243d38b362cecfb4087
vadzim.asipovich <deathrezen@gmail.com>
2024-03-16 09:54:16 +0100

    delete constructor test

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -6,13 +6,6 @@ import static org.junit.Assert.*;
 public class BunTest {
 
     @Test
-    public void testConstructor() {
-        Bun bun = new Bun("Plain Bun", 1.50f);
-        assertEquals("Plain Bun", bun.getName());
-        assertEquals(1.50f, bun.getPrice(), 0.01f); // Delta is set to handle floating-point comparison
-    }
-
-    @Test
     public void testGetName() {
         Bun bun = new Bun("Sesame Bun", 2.00f);
         assertEquals("Sesame Bun", bun.getName());
