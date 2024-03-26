56665e04476dd6ec085ae03a4bc614be67be29b9
Nadezhda Sokolova <Nadegda1V@yandex.ru>
2024-03-26 23:08:40 +0300

    checkInitializationBun + checkGetPrice in BurgerTest

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -2,16 +2,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 
@@ -24,10 +19,14 @@ public class BurgerTest {
    @Mock
     Ingredient ingredient;
 
+    @Test
+    public void checkInitializationBun(){
+        Burger burger = new Burger();
+        Bun bun = new Bun("Краторная булка N-200i", 1255);
+        burger.setBuns(bun);
+        assertEquals(bun, burger.bun);
 
-    public List<Ingredient> ingredients = new ArrayList<>();
-
-
+    }
    @Test
    public void checkAddingIngredient(){
        Burger burger = new Burger();
@@ -47,15 +46,36 @@ public class BurgerTest {
         assertEquals(before-1, after);
     }
 
+    @Test
 
-//    @Test
-//    public void checkGetPrice(){
-//        Burger burger = new Burger();
-//        Mockito.when(bun.getPrice()).thenReturn(1.1F);
-//        Mockito.when(ingredients.getPrice()).thenReturn(2.0F);
-//        assertEquals(24,burger.getPrice(),0);
-//
-//    }
+    public void checkMoveIngredient(){
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+        burger.addIngredient(new Ingredient(IngredientType.FILLING, "sausage", 300));
+        burger.moveIngredient(0,1);
+        burger.ingredients.get(0).equals(new Ingredient(IngredientType.FILLING, "sausage", 300));
 
+    }
+
+
+    @Test
+    public void checkGetPrice(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Mockito.when(bun.getPrice()).thenReturn(1.1F);
+        Mockito.when(ingredient.getPrice()).thenReturn(2.0F);
+        assertEquals(4.2,burger.getPrice(),0.1);
+
+    }
+
+    @Test
+    public void getReceipt() {
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(new Ingredient(IngredientType.FILLING, "sausage", 300));
+        Mockito.when(bun.getName()).thenReturn("white bun");
+        assertEquals("(==== white bun ====)\n= filling sausage =\n(==== white bun ====)\n\nPrice: 300,000000\n",burger.getReceipt());
+    }
 
 }
