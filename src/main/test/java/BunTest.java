2b2ce86acfdfe7fbb2e1b09f669fe45ee0013d10
VLAD <kosyackoff.vlad@yandex.ru>
2024-03-28 02:13:42 +0300

    Добавил тесты в классу Burger

--- src/main/test/java/BunTest.java
+++ src/main/test/java/BunTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class BunTest {
+import org.junit.Assert;
+import org.junit.Test;
+import praktikum.Bun;
+
+public class BunTest {
+
+    Bun bun = new Bun("white", 104.5F);
+
+    @Test
+    public void testGetBunName(){
+        Assert.assertEquals("white", bun.getName());
+    }
+
+    @Test
+    public void testGetBunPrice(){
+        Assert.assertEquals(104.5F,bun.getPrice(),0);
+    }
 }
